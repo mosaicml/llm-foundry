842654256959c298c7d7e20144a62ccd8a863aa6
Landan Seguin <landan@mosaicml.com>
2022-11-22 11:33:33 -0800

    ResNet Benchmark (#25)
    Updates the ResNet benchmark to no longer use yahp.
    
    Co-authored-by: Matthew <growlix@users.noreply.github.com>
    Co-authored-by: dblalock <dwb4ke@virginia.edu>

---
+++ resnet/tests/utils.py
@@ -0,0 +1,34 @@
+import itertools
+import os
+import tempfile
+import shutil
+
+import numpy as np
+from PIL import Image
+
+class SynthClassificationDirectory(object):
+
+    def __enter__(self):
+        self.path = create_synthetic_imagenet()
+        return self.path
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        shutil.rmtree(self.path)
+
+def create_synthetic_imagenet():
+    tmp_dirname = tempfile.mkdtemp()
+
+    # Make train and val directories with a few classes, then add images
+    splits = ['train', 'val']
+    labels = ['dog', 'cat', 'person']
+    n_img_per_class = 4
+    for split, label in itertools.product(splits, labels):
+        dirpath = os.path.join(tmp_dirname, split, label)
+        os.makedirs(dirpath)
+
+        for i in range(n_img_per_class):
+            arr = np.random.rand(32, 32, 3) * 255
+            img = Image.fromarray(arr.astype('uint8')).convert('RGB')
+            img.save(os.path.join(dirpath, f'img{i}.jpeg'))
+
+    return tmp_dirname

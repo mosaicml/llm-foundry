c0a648979ccef39e769ffca580be9a9904d2f0a7
Austin <A-Jacobson@users.noreply.github.com>
2022-11-23 15:11:43 -0800

    Ade20k benchmark (#27)

---
+++ deeplab/download_ade20k.py
@@ -0,0 +1,33 @@
+# Copyright 2022 MosaicML Composer authors
+# SPDX-License-Identifier: Apache-2.0
+"""ADE20K Semantic segmentation and scene parsing dataset.
+Please refer to the `ADE20K dataset <https://groups.csail.mit.edu/vision/datasets/ADE20K/>`_ for more details about this
+dataset.
+"""
+
+import argparse
+import torchvision
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    'path',
+    help='ADE20k Download directory.',
+    type=str)
+
+args = parser.parse_args()
+
+ADE20K_URL = 'http://data.csail.mit.edu/places/ADEchallenge/ADEChallengeData2016.zip'
+ADE20K_FILE = 'ADEChallengeData2016.zip'
+
+
+def main():
+    torchvision.datasets.utils.download_and_extract_archive(
+        url=ADE20K_URL,
+        download_root=args.path,
+        filename=ADE20K_FILE,
+        remove_finished=True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ deeplab/tests/test_trainer.py
@@ -0,0 +1,50 @@
+import os
+import sys
+
+import pytest
+
+# TODO: this should be removed when benchmarks has a setup.py i.e. installable
+sys.path.append('.')
+
+import torch
+from omegaconf import OmegaConf
+
+from ..main import main
+from ..tests.test_data import SynthADE20KDirectory
+
+
+@pytest.mark.parametrize('recipe_name', [None, 'mild', 'medium', 'hot'])
+def test_trainer(recipe_name):
+    with open('yamls/deeplabv3.yaml') as f:
+        base_config = OmegaConf.load(f)
+
+    with open('tests/smoketest_config.yaml') as f:
+        smoke_config = OmegaConf.load(f)
+
+    config = OmegaConf.merge(base_config, smoke_config)
+    config.recipe_name = recipe_name
+
+    with SynthADE20KDirectory() as tmp_datadir:
+        print(tmp_datadir)
+        config.train_dataset.path = tmp_datadir
+        config.eval_dataset.path = tmp_datadir
+        # Also save checkpoints in the temporary directory
+        config.save_folder = tmp_datadir
+
+        # Train
+        trainer1 = main(config)
+        model1 = trainer1.state.model.module
+
+        # Check that the checkpoint was saved
+        chkpt_path = os.path.join(tmp_datadir, 'ep0-ba1-rank0.pt')
+        assert os.path.isfile(chkpt_path)
+
+        # Check that the checkpoint was loaded by comparing model weights
+        config.load_path = chkpt_path
+        config.is_train = False
+        config.seed += 10  # change seed
+        trainer2 = main(config)
+        model2 = trainer2.state.model.module
+
+        for param1, param2 in zip(model1.parameters(), model2.parameters()):
+            torch.testing.assert_close(param1, param2)

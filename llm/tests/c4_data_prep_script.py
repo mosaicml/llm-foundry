668050f253bf036376b79148036b0bb107b11c67
Vitaliy Chiley <vitaliy@mosaicml.com>
2022-11-29 09:37:04 -0800

    Vitaliy/sync foundry tests (#32)
    * porting foundry tests
    
    * rm unittest
    
    * xfail test when dataset is not set up
    
    Co-authored-by: Vitaliy Chiley <vitaliy@moasic.com>

---
+++ llm/tests/c4_data_prep_script.py
@@ -0,0 +1,22 @@
+# Copyright 2022 MosaicML Benchmarks authors
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import shutil
+from argparse import Namespace
+
+from convert_c4 import main
+
+
+def test_download_script_from_api():
+    # test calling it directly
+    main(Namespace(**{'splits': ['val'], 'out_root': './my-copy-c4-1'}))
+    assert os.path.exists(os.path.join(os.getcwd(), 'my-copy-c4-1'))
+    shutil.rmtree(os.path.join(os.getcwd(), 'my-copy-c4-1'))
+
+
+def test_download_script_from_cmdline():
+    # test calling it via the cmd line interface
+    os.system("python convert_c4.py --out_root ./my-copy-c4-2 --splits val")
+    assert os.path.exists(os.path.join(os.getcwd(), 'my-copy-c4-2'))
+    shutil.rmtree(os.path.join(os.getcwd(), 'my-copy-c4-2'))

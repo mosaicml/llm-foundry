4390ca059c81d4a837caa80030f07ee7bd0d2f0c
Sam Havens <samhavens@gmail.com>
2022-12-15 20:44:36 -0800

    embedding fraction implementation (#43)
    * embedding fraction implementation
    
    * fail if bad value of embedding_fraction
    
    * attribute GLM-130B for implementation

--- llm/src/mosaic_gpt.py
+++ llm/src/mosaic_gpt.py
@@ -145,6 +145,10 @@ class MosaicGPT(nn.Module):
         super().__init__()
         assert cfg.name == 'mosaic_gpt', f'Tried to build MosaicGPT model with cfg.name={cfg.name}'
         self.cfg = cfg
+        # CogView (https://arxiv.org/abs/2105.13290) and GLM-130B (https://arxiv.org/abs/2210.02414)
+        # both report this helping with stabilizing training
+        self.embedding_fraction = cfg.get("embedding_fraction", 1)
+        assert 0 < self.embedding_fraction <= 1, "model.embedding_fraction must be between 0 (exclusive) and 1 (inclusive)!"
         self.transformer = nn.ModuleDict(
             dict(
                 wte=nn.Embedding(cfg.vocab_size, cfg.d_model,
@@ -186,7 +190,14 @@ class MosaicGPT(nn.Module):
 
         tok_emb = self.transformer.wte(input_ids)  # type: ignore
         pos_emb = self.transformer.wpe(pos)  # type: ignore
-        x = self.transformer.emb_drop(tok_emb + pos_emb)  # type: ignore
+        if self.embedding_fraction == 1:
+            x = self.transformer.emb_drop(tok_emb + pos_emb)  # type: ignore
+        else:
+            x = tok_emb + pos_emb
+            # this implementation is proposed on page 7 of the GLM-130B paper https://arxiv.org/abs/2210.02414
+            x = self.transformer.emb_drop(
+                x * self.embedding_fraction + x.detach() * (1 - self.embedding_fraction)
+            )
         for block in self.transformer.blocks:  # type: ignore
             x = block(x, key_padding_mask)
         x = self.transformer.ln_f(x)  # type: ignore

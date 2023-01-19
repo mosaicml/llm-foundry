cc1ec74c05d8c3d0facdd8b8fdc444808b215c5c
dblalock <davis@mosaicml.com>
2023-01-19 15:22:50 -0800

    Make repo pip installable (#76)
    * make repo pip installable / rename common->mosaicml_examples
    
    * fix precommit config scattering pyright output
    
    * autoformatting

--- llm/main.py
+++ llm/main.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import os
-import pathlib
 import sys
 import warnings
 
@@ -11,11 +10,10 @@ from composer.utils import dist, reproducibility
 from omegaconf import OmegaConf as om
 from src.model_registry import COMPOSER_MODEL_REGISTRY
 
-sys.path.append(str(pathlib.Path(__file__).parent.parent / 'common'))
-from builders import build_dataloader  # type: ignore (reportMissingImports)
-from builders import (build_algorithm, build_callback, build_logger,
-                      build_optimizer, build_scheduler)
-from logging_utils import log_config  # type: ignore (reportMissingImports)
+from mosaicml_examples.builders import (build_algorithm, build_callback,
+                                        build_dataloader, build_logger,
+                                        build_optimizer, build_scheduler)
+from mosaicml_examples.logging_utils import log_config
 
 
 def calculate_batch_size_info(global_batch_size, device_microbatch_size):
@@ -84,7 +82,8 @@ def main(cfg):
                                   resolve=True) if fsdp_config else None
 
     # Restrict model init device to 'meta' and 'cpu',
-    # using 'cuda' vs. 'cuda:id' is tricky and can lead to common user errors when multiple GPUs are available.
+    # using 'cuda' vs. 'cuda:id' is tricky and can lead to common user errors
+    # when multiple GPUs are available.
     # Also 'meta' is only valid when using FSDP
     assert cfg.model.device in ['meta', 'cpu']
     if fsdp_config is None and cfg.model.device == 'meta':
---
+++ mosaicml_examples/__init__.py
@@ -0,0 +1,14 @@
+# Copyright 2022 MosaicML Examples authors
+# SPDX-License-Identifier: Apache-2.0
+
+__version__ = '0.0.1'
+
+from . import builders, logging_utils, speed_monitor_w_mfu, text_data
+
+__all__ = [
+    '__version__',
+    'builders',
+    'text_data',
+    'speed_monitor_w_mfu',
+    'logging_utils',
+]

70f07314ca8f27f121d511af2d73bfbb0d019523
Vitaliy Chiley <6439018+vchiley@users.noreply.github.com>
2023-02-22 20:26:43 -0800

    refactor file structure and attn mask gen fn (#187)
    moving layers into models folder; updt test; mv attn mask gen fn

--- examples/llm/src/models/layers/__init__.py
+++ examples/llm/src/models/layers/__init__.py
@@ -1,35 +1,20 @@
 # Copyright 2022 MosaicML Examples authors
 # SPDX-License-Identifier: Apache-2.0
 
-from examples.llm.src.model_registry import COMPOSER_MODEL_REGISTRY
-from examples.llm.src.models.hf import (ComposerHFCausalLM, ComposerHFPrefixLM,
-                                        ComposerHFT5)
 from examples.llm.src.models.layers.attention import (
     FlashCausalAttention, TorchCausalAttention, TritonFlashCausalAttention,
     alibi_bias)
 from examples.llm.src.models.layers.flash_attention import (FlashAttention,
                                                             FlashMHA)
 from examples.llm.src.models.layers.gpt_blocks import GPTMLP, GPTBlock
-from examples.llm.src.models.mosaic_gpt import ComposerMosaicGPT, MosaicGPT
-from examples.llm.src.tokenizer import (TOKENIZER_REGISTRY, HFTokenizer,
-                                        LLMTokenizer)
 
 __all__ = [
     'FlashAttention',
     'FlashMHA',
-    'ComposerHFCausalLM',
-    'ComposerHFPrefixLM',
-    'ComposerHFT5',
-    'COMPOSER_MODEL_REGISTRY',
     'TorchCausalAttention',
     'FlashCausalAttention',
     'TritonFlashCausalAttention',
     'alibi_bias',
     'GPTMLP',
     'GPTBlock',
-    'MosaicGPT',
-    'ComposerMosaicGPT',
-    'LLMTokenizer',
-    'HFTokenizer',
-    'TOKENIZER_REGISTRY',
 ]

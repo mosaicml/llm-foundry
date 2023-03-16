9bceef3b068a5977c8c3303d21820f246cf97f4b
Vitaliy Chiley <6439018+vchiley@users.noreply.github.com>
2023-03-16 11:51:30 -0700

    Fdiff updt (#241)
    * updt fdiff to only plot fdiff if it has seen metric before
    
    * dk pr cmt

--- examples/common/fdiff.py
+++ examples/common/fdiff.py
@@ -7,8 +7,6 @@
 """Monitor rate of change of loss."""
 from __future__ import annotations
 
-from typing import Any, Dict
-
 from composer.core import Callback, State
 from composer.loggers import Logger
 
@@ -16,11 +14,11 @@ from composer.loggers import Logger
 class FDiffMetrics(Callback):
     """Rate of chage of metrics.
 
-    tracks and plots the rate of change of metrics effectively taking the numerical
-    derivative of the metrics
+    tracks and plots the rate of change of metrics effectively taking the
+    numerical derivative of the metrics
     """
 
-    def __init__(self, diff_train_metrics=True, diff_eval_metrics=True):
+    def __init__(self, diff_train_metrics=False, diff_eval_metrics=True):
         self.diff_train_metrics = diff_train_metrics
         self.diff_eval_metrics = diff_eval_metrics
 
@@ -38,26 +36,31 @@ class FDiffMetrics(Callback):
 
             self.train_prev_loss = state.loss.item()
 
-            if self.train_prev_metric:
-                for k in state.train_metric_values.keys():
-                    logger.log_metrics({
-                        f'metrics/train/{k}_fdiff':
-                            state.train_metric_values[k].item() -
-                            self.train_prev_metric[k]
-                    })
+            for k in self.train_prev_metric.keys():
+                logger.log_metrics({
+                    f'metrics/train/{k}_fdiff':
+                        state.train_metric_values[k].item() -
+                        self.train_prev_metric[k]
+                })
 
             for k in state.train_metric_values.keys():
                 self.train_prev_metric[k] = state.train_metric_values[k].item()
 
     def eval_end(self, state: State, logger: Logger):
         if self.diff_eval_metrics:
-            if self.eval_prev_metric:
-                for k in state.eval_metric_values.keys():
+            evaluator = state.dataloader_label
+            metrics = list(state.eval_metrics[evaluator].keys())
+
+            for k in metrics:
+                mkey = '/'.join(['metrics', evaluator, k])
+                if mkey in self.eval_prev_metric.keys():
                     logger.log_metrics({
-                        f'metrics/eval/{k}_fdiff':
+                        f'{mkey}_fdiff':
                             state.eval_metric_values[k].item() -
-                            self.eval_prev_metric[k]
+                            self.eval_prev_metric[mkey]
                     })
 
-            for k in state.eval_metric_values.keys():
-                self.eval_prev_metric[k] = state.eval_metric_values[k].item()
+            for k in metrics:
+                mkey = '/'.join(['metrics', evaluator, k])
+                value = state.eval_metric_values[k].item()
+                self.eval_prev_metric[mkey] = value

c5c0464311ba2886adacc17038929c35b54db33f
Sam Havens <sam@mosaicml.com>
2023-04-06 16:22:43 -0700

    Revert "make LlamaTokenizer work for tokenizer bakeoff" (#284)

--- examples/llm/src/models/utils/__init__.py
+++ examples/llm/src/models/utils/__init__.py
@@ -5,8 +5,6 @@ from examples.llm.src.models.utils.adapt_tokenizer import (
     AutoTokenizerForMOD, adapt_tokenizer_for_denoising)
 from examples.llm.src.models.utils.hf_prefixlm_converter import (
     add_bidirectional_mask_if_missing, convert_hf_causal_lm_to_prefix_lm)
-from examples.llm.src.models.utils.make_llama_work_with_autoclass import \
-    make_llama_work
 from examples.llm.src.models.utils.meta_init_context import init_empty_weights
 from examples.llm.src.models.utils.param_init_fns import (  # type: ignore
     MODEL_INIT_REGISTRY, generic_param_init_fn_)
@@ -15,5 +13,5 @@ __all__ = [
     'AutoTokenizerForMOD', 'adapt_tokenizer_for_denoising',
     'convert_hf_causal_lm_to_prefix_lm', 'init_empty_weights',
     'add_bidirectional_mask_if_missing', 'generic_param_init_fn_',
-    'MODEL_INIT_REGISTRY', 'make_llama_work'
+    'MODEL_INIT_REGISTRY'
 ]

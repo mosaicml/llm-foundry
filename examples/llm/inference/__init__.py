2eb0fa2b3e80922d9d891a95a241205f98ccfbec
Abhi Venigalla <77638579+abhi-mosaic@users.noreply.github.com>
2023-04-06 18:26:14 -0700

    Add HF conversion and generate scripts (#277)

--- examples/llm/inference/__init__.py
+++ examples/llm/inference/__init__.py
@@ -1,8 +1,4 @@
 # Copyright 2022 MosaicML Examples authors
 # SPDX-License-Identifier: Apache-2.0
 
-from examples.llm.inference.inference import get_mosaicgpt_inference_model
-
-__all__ = [
-    'get_mosaicgpt_inference_model',
-]
+__all__ = []

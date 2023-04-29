31d679a67c431e80e63ab3294de82981c678ee94
Mihir Patel <mihir.v.patel7@gmail.com>
2023-04-28 17:02:56 -0700

    checkdown

--- llmfoundry/common/optim/__init__.py
+++ llmfoundry/common/optim/__init__.py
@@ -1,11 +1,8 @@
 # Copyright 2022 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
-# Copyright 2022 MosaicML Examples authors
-# SPDX-License-Identifier: Apache-2.0
-
-from examples.common.optim.adaptive_lion import (DecoupledAdaLRLion,
+from llmfoundry.common.optim.adaptive_lion import (DecoupledAdaLRLion,
                                                  DecoupledClipLion)
-from examples.common.optim.lion import DecoupledLionW
+from llmfoundry.common.optim.lion import DecoupledLionW
 
 __all__ = ['DecoupledLionW', 'DecoupledClipLion', 'DecoupledAdaLRLion']

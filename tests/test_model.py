7cdee6bbb8cb6f6105fa8558e237a2b9dfdaea2e
Mihir Patel <mihir.v.patel7@gmail.com>
2023-04-28 17:29:00 -0700

    patch test

--- tests/test_model.py
+++ tests/test_model.py
@@ -188,7 +188,7 @@ def test_full_forward_and_backward_gpt2_small(prefixlm, batch_size=2):
     warnings.filterwarnings(
         action='ignore',
         message='Torchmetrics v0.9 introduced a new argument class property')
-    conf_path = 'yamls/hf_causal_lm/gpt2-small.yaml'
+    conf_path = 'llmfoundry/yamls/hf_causal_lm/gpt2-small.yaml'
     with open(conf_path) as f:
         neo_cfg = om.load(f)
 

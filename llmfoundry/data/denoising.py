c8b00a815c0128437bd4dc9aae04b808bfd1ee5c
Abhi Venigalla <77638579+abhi-mosaic@users.noreply.github.com>
2023-05-02 17:10:39 -0700

    Make ICL configurations more concise (#13)

--- llmfoundry/data/denoising.py
+++ llmfoundry/data/denoising.py
@@ -476,7 +476,7 @@ def build_text_denoising_dataloader(
         download_timeout=cfg.dataset.get('download_timeout', 60),
         validate_hash=cfg.dataset.get('validate_hash'),
         shuffle_seed=cfg.dataset.get('shuffle_seed'),
-        num_canonical_nodes=cfg.dataset.get('num_canonical_nodes'),
+        num_canonical_nodes=cfg.dataset.get('num_canonical_nodes', 128),
         batch_size=device_batch_size)
 
     if dataset.tokenizer.pad_token is None:  # type: ignore

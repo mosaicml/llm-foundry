81ee2c31011cc5180cac810aeb8bb3500fa59d6e
Abhinav Venigalla <abhi@mosaicml.com>
2023-05-04 19:21:14 +0000

    fix race

--- llmfoundry/utils/builders.py
+++ llmfoundry/utils/builders.py
@@ -9,6 +9,7 @@ from composer.callbacks import (HealthChecker, LRMonitor, MemoryMonitor,
                                 OptimizerMonitor, RuntimeEstimator,
                                 SpeedMonitor)
 from composer.core import Evaluator
+from composer.utils import dist
 from composer.datasets.in_context_learning_evaluation import \
     get_icl_task_dataloader
 from composer.loggers import WandBLogger
@@ -212,6 +213,9 @@ def build_icl_evaluators(icl_tasks,
             metric_names = list(icl_cfg.metric_names)
             # TODO: fix Composer bug when copying local paths and destination exists
             destination_path = f'{destination_dir}/{icl_cfg.label}-{num_fewshot}.jsonl'
+            with dist.run_local_rank_zero_first():
+                if os.path.exists(destination_path):
+                    os.remove(destination_path)
             dataloaders = get_icl_task_dataloader(
                 icl_cfg.icl_task_type,
                 icl_cfg.dataset_uri,

9e700065e321d45eb1ed3a0dec64bd3dec3348fe
Cody <cody@mosaicml.com>
2023-05-05 00:28:22 +0000

    pre commit

--- tests/test_data_prep_scripts.py
+++ tests/test_data_prep_scripts.py
@@ -9,9 +9,9 @@ from argparse import Namespace
 # Add repo root to path so we can import scripts and test it
 repo_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(repo_dir)
+from scripts.data_prep.convert_dataset_csv import main as main_csv
 from scripts.data_prep.convert_dataset_hf import main as main_hf
 from scripts.data_prep.convert_dataset_json import main as main_json
-from scripts.data_prep.convert_dataset_csv import main as main_csv
 
 
 def test_download_script_from_api():
@@ -77,6 +77,7 @@ def test_json_script_from_cmdline():
     assert os.path.exists(path)
     shutil.rmtree(path, ignore_errors=False)
 
+
 def test_csv_script_from_api():
     # test calling it directly
     path = os.path.join(os.getcwd(), 'my-copy-c4-1')
@@ -106,4 +107,3 @@ def test_csv_script_from_cmdline():
     )
     assert os.path.exists(path)
     shutil.rmtree(path, ignore_errors=False)
-

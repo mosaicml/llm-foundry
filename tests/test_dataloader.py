3b7a5a383e6b9b95bd10a146af5280ed3b1a2276
Mihir Patel <mihir.v.patel7@gmail.com>
2023-05-04 23:02:59 -0700

    remove

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -46,6 +46,8 @@ def test_correct_padding(tokenizer_name, pretokenize, batch_size=4):
 
     data_local = get_data_local(tokenizer_name, pretokenize)
     split = 'val_xsmall'
+    eos_text = None
+    bos_text = None
     if tokenizer_name == 'gpt2':
         eos_text = '<|endoftext|>'
     elif tokenizer_name == 'facebook/opt-125m':

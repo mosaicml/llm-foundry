28e180a7d34c0ae63d05aaf27b487220bf10aec4
Mihir Patel <mihir.v.patel7@gmail.com>
2023-05-09 14:26:04 -0700

    fix tweaks

--- setup.py
+++ setup.py
@@ -89,7 +89,6 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/mosaicml/llm-foundry/',
-    include_package_data=True,
     package_data={
         'llmfoundry': ['py.typed'],
     },

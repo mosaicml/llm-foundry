22e4590de0731c7ba3f352f89c1a203f26517b24
Alex Trott <alex@mosaicml.com>
2023-05-11 14:17:06 -0700

    wip

---
+++ scripts/train/finetune_example/preprocessing.py
@@ -0,0 +1,55 @@
+# Copyright 2022 MosaicML LLM Foundry authors
+# SPDX-License-Identifier: Apache-2.0
+
+r"""Example custom preprocessing function.
+
+This is here to help illustrate the way to set up finetuning
+on a local dataset. One step of that process is to create
+a preprocessing function for your dataset, and that is what
+is done below. Check out the LLM Finetuning section of
+`../README.md` for more context.
+
+For this example, we're going to pretend that our local dataset
+is `./train.jsonl`.
+
+Note: this dataset is actually a copy of one of our ARC-Easy
+multiple-choice ICL eval datasets. And you would never actually
+train on eval data! ... But this is just a demonstration.
+
+Every example within the dataset has the format:
+{
+    'query': <query text>,
+    'choices': [<choice 0 text>, <choice 1 text>, ...],
+    'gold': <int> # index of correct choice
+}
+
+To enable finetuning, we want to turn this into a prompt/response
+format. We'll structure prompts and responses like this:
+{
+    'prompt': <query text>\nOptions:\n - <choice 0 text>\n - <choice 1 text>\nAnswer: ,
+    'response': <correct choice text>
+}
+"""
+
+from typing import Dict
+
+
+def multiple_choice(inp: Dict[str, str]) -> Dict[str, str]:
+    PROMPT_FORMAT = '{query}\nOptions:{options}\nAnswer: '
+    options = ''
+    for option in inp['choices']:
+        options += f'\n - {option}'
+    query = inp['query']
+    return {
+        'prompt': PROMPT_FORMAT.format(query=query, options=options),
+        'response': inp['choices'][inp['gold']],
+    }
+
+
+# import datasets
+# mc_dataset = datasets.load_dataset('json', data_files={'train': '../eval/local_data/arc_easy.jsonl'}, split='train')
+# raw_example = mc_dataset[0]
+# print(f'Before...\n{raw_example}\n\n')
+# processed_example = multiple_choice(raw_example)
+# print(f'After...\n{processed_example}\n\n')
+# print(f'-------\n"prompt"=\n{processed_example["prompt"]}\n\n"response"=\n{processed_example["response"]}\n')

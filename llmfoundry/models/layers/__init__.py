d5ed7ebdc192db2935a82fa9b1c025e47c58e96a
Vitaliy Chiley <vitaliyachiley@gmail.com>
2023-05-16 21:39:34 +0000

    lint

--- llmfoundry/models/layers/__init__.py
+++ llmfoundry/models/layers/__init__.py
@@ -1,11 +1,11 @@
 # Copyright 2022 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
+from llmfoundry.models.layers import flash_attn_triton
 from llmfoundry.models.layers.attention import (
     ATTN_CLASS_REGISTRY, MultiheadAttention, MultiQueryAttention,
     attn_bias_shape, build_alibi_bias, build_attn_bias, flash_attn_fn,
     scaled_multihead_dot_product_attention, triton_flash_attn_fn)
-from llmfoundry.models.layers import flash_attn_triton
 from llmfoundry.models.layers.blocks import MPTMLP, MPTBlock
 from llmfoundry.models.layers.norm import NORM_CLASS_REGISTRY, LPLayerNorm
 

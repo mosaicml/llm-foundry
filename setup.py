39cbc72b3419085da9c2319ed5c410b922833977
Vitaliy Chiley <vitaliyachiley@gmail.com>
2023-05-17 22:57:27 +0000

    updt

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ classifiers = [
 install_requires = [
     'composer[nlp,wandb]>=0.14.1,<0.15',
     'mosaicml-streaming>=0.4.1,<0.5',
-    'torch==2.0.1',
+    'torch>=1.13.1,<=2.0.1',
     'datasets==2.10.1',
     'sentencepiece==0.1.97',
     'einops==0.5.0',

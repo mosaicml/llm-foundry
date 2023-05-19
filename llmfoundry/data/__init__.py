f4b158a1b9e8fcea98359e84f3ed2c9cbbeaf289
Hanlin Tang <hanlin@mosaicml.com>
2023-05-19 08:48:06 -0700

    rename datasets to avoid hf conflict

--- llmfoundry/data/__init__.py
+++ llmfoundry/data/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2022 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
-from llmfoundry.data.datasets import ConcatTokensDataset, NoConcatDataset
+from llmfoundry.data.data import ConcatTokensDataset, NoConcatDataset
 from llmfoundry.data.denoising import (MixtureOfDenoisersCollator,
                                        build_text_denoising_dataloader)
 from llmfoundry.data.finetuning import (Seq2SeqFinetuningCollator,

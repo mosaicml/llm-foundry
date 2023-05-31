5c1e99720a79947d5aef6b81004697d6e358021c
Nikhil <101217697+nik-mosaic@users.noreply.github.com>
2023-05-31 11:18:22 -0700

    bump flashattention model

--- llmfoundry/models/layers/attention.py
+++ llmfoundry/models/layers/attention.py
@@ -151,7 +151,7 @@ def flash_attn_fn(
     try:
         from flash_attn import bert_padding, flash_attn_interface  # type: ignore # yapf: disable # isort: skip
     except:
-        raise RuntimeError('Please install flash-attn==1.0.3.post0')
+        raise RuntimeError('Please install flash-attn==1.0.7.post0')
 
     check_valid_inputs(query, key, value)
 
@@ -249,7 +249,7 @@ def triton_flash_attn_fn(
         if version.parse(torch.__version__) < version.parse('2.0.0'):
             _installed = True
             # if torch1.13.1 revert to using triton flash attn from HazyResearch
-            # with flash-attn==1.0.3.post0 and triton==2.0.0.dev20221202
+            # with flash-attn==1.0.7.post0 and triton==2.0.0.dev20221202
             try:
                 from flash_attn.flash_attn_triton import flash_attn_func
             except:

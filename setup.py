1ec21846c765c93eb94d12ac2abbbdb2a226969c
Abhi Venigalla <77638579+abhi-mosaic@users.noreply.github.com>
2023-06-20 17:16:55 -0600

    Upgrade to `mosaicml-streaming==0.5.x` (#292)
    * Changes to support streaming 050
    
    * Renamed samples to choose in stream
    
    * Create tasks-light.yaml
    
    * Rename tasks-light.yaml to tasks_light.yaml
    
    * remove tasks_light
    
    * upgrade streaming to 050
    
    * Update llmfoundry/data/text_data.py
    
    Co-authored-by: Vitaliy Chiley <6439018+vchiley@users.noreply.github.com>
    
    * updt to stream 0.5.1
    
    * enable kwargs to pass to init direct
    
    ---------
    
    Co-authored-by: Karan Jariwala <karankjariwala@gmail.com>
    Co-authored-by: Jonathan Frankle <jonathanfrankle@gmail.com>
    Co-authored-by: Vitaliy Chiley <6439018+vchiley@users.noreply.github.com>
    Co-authored-by: Vitaliy Chiley <vitaliy@mosaicml.com>

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ classifiers = [
 install_requires = [
     'composer[libcloud,nlp,wandb]>=0.14.1,<0.15',
     'accelerate>=0.19,<0.20',  # for HF inference `device_map`
-    'mosaicml-streaming>=0.4.1,<0.5',
+    'mosaicml-streaming>=0.5.1,<0.6',
     'torch>=1.13.1,<=2.0.1',
     'datasets==2.10.1',
     'sentencepiece==0.1.97',

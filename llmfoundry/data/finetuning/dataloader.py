0139a458890e5fa577bd1e3894c12e075b03da82
root <vitaliy@mosaicml.com>
2023-06-22 04:08:29 +0000

    updt

--- llmfoundry/data/finetuning/dataloader.py
+++ llmfoundry/data/finetuning/dataloader.py
@@ -183,10 +183,17 @@ def build_finetuning_dataloader(cfg: DictConfig, tokenizer: Tokenizer,
                     cfg.dataset['hf_kwargs'] = kwargs
                     print(cfg.dataset)
                     dataset = dataset_constructor.build_from_hf(
-                        cfg.dataset, tokenizer=tokenizer)
+                        cfg.dataset,
+                        max_seq_len=cfg.dataset.max_seq_len,
+                        tokenizer=tokenizer,
+                    )
                     break
         else:
-            dataset = dataset_constructor.build_from_hf(cfg.dataset, tokenizer=tokenizer)
+            dataset = dataset_constructor.build_from_hf(
+                cfg.dataset,
+                max_seq_len=cfg.dataset.max_seq_len,
+                tokenizer=tokenizer,
+            )
 
         collate_fn, dataloader_batch_size = _build_collate_fn(
             cfg.dataset, tokenizer, device_batch_size)

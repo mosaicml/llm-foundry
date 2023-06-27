9b3ae8efaa8da809ef9c5917df93cc6276bb73c0
danbider <dan@mosaicml.com>
2023-06-27 17:54:52 +0000

    scipy dep for bitsandbytes

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ extra_deps['gpu'] = [
     'loralib==0.1.1',  # lora core
     'peft @ git+https://github.com/huggingface/peft.git',  # TODO: pin it down only after it stabilizes.
     'bitsandbytes==0.39.1',  # 8bit
+    'scipy==1.11.0',  # bitsandbytes dependency; TODO: eliminate when incorporated to bitsandbytes
 ]
 
 extra_deps['all'] = set(dep for deps in extra_deps.values() for dep in deps)

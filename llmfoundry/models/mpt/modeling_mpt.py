dea59aaf47830e00116fc09a30af61287f0c4afc
Hanlin Tang <hanlin@mosaicml.com>
2023-06-27 12:59:56 -0700

    Fix Typing (part 1) (#240)
    * remove some type ignore
    
    * fix typing
    
    * fix formatting
    
    * pr cmts
    
    ---------
    
    Co-authored-by: Vitaliy Chiley <vitaliy@mosaicml.com>

--- llmfoundry/models/mpt/modeling_mpt.py
+++ llmfoundry/models/mpt/modeling_mpt.py
@@ -33,21 +33,31 @@ from llmfoundry.models.layers.blocks import MPTBlock
 from llmfoundry.models.layers.custom_embedding import SharedEmbedding
 from llmfoundry.models.layers.norm import NORM_CLASS_REGISTRY
 from llmfoundry.models.mpt.configuration_mpt import MPTConfig
-# NOTE: We import all the utils directly just so that HuggingFace will detect
-# all the files that it needs to copy into its modules folder. Otherwise it misses
-# the ones imported in the submodule
+
+# NOTE: All utils are imported directly even if unused so that
+# HuggingFace can detect all the needed files to copy into its modules folder.
+# Otherwise, certain modules are missing.
+# isort: off
 from llmfoundry.models.utils.adapt_tokenizer import (
-    AutoTokenizerForMOD, adapt_tokenizer_for_denoising)
+    AutoTokenizerForMOD,  # type: ignore (see note),
+    adapt_tokenizer_for_denoising,  # type: ignore (see note)
+)
 from llmfoundry.models.utils.hf_prefixlm_converter import (
-    add_bidirectional_mask_if_missing, convert_hf_causal_lm_to_prefix_lm)
-from llmfoundry.models.utils.meta_init_context import init_empty_weights
-from llmfoundry.models.utils.param_init_fns import (  # type: ignore
-    MODEL_INIT_REGISTRY, generic_param_init_fn_)
+    add_bidirectional_mask_if_missing,  # type: ignore (see note)
+    convert_hf_causal_lm_to_prefix_lm,  # type: ignore (see note)
+)
+from llmfoundry.models.utils.meta_init_context import \
+    init_empty_weights  # type: ignore (see note)
+from llmfoundry.models.utils.param_init_fns import (
+    generic_param_init_fn_,  # type: ignore (see note)
+    MODEL_INIT_REGISTRY,
+)
 
 try:
     from llmfoundry.models.layers.flash_attn_triton import flash_attn_func
 except:
     pass
+# isort: on
 
 Tokenizer = Union[PreTrainedTokenizer, PreTrainedTokenizerFast]
 
@@ -145,7 +155,7 @@ class MPTModel(MPTPreTrainedModel):
     def get_input_embeddings(self):
         return self.wte
 
-    def set_input_embeddings(self, value):
+    def set_input_embeddings(self, value: nn.Embedding):
         self.wte = value
 
     @torch.no_grad()
@@ -294,6 +304,7 @@ class MPTModel(MPTPreTrainedModel):
 
         if attention_mask is not None:
             attention_mask = attention_mask.bool()
+
         if prefix_mask is not None:
             prefix_mask = prefix_mask.bool()
 
@@ -320,8 +331,7 @@ class MPTModel(MPTPreTrainedModel):
                 'prefix_mask is a required argument when MPT is configured with prefix_lm=True.'
             )
 
-
-# Raise a not implemented error if input_embeds is not None (this is an arg in huggingface transformers and we need to support it for PEFT)
+        # Raise a not implemented error if input_embeds is not None (this is an arg in huggingface transformers and we need to support it for PEFT)
         if inputs_embeds is not None:
             raise NotImplementedError(
                 'inputs_embeds is not implemented for MPT.')
@@ -473,7 +483,7 @@ class MPTForCausalLM(MPTPreTrainedModel):
 
         print(f'Instantiating an MPTForCausalLM model from {__file__}')
 
-        self.transformer = MPTModel(config)
+        self.transformer: MPTModel = MPTModel(config)
 
         for child in self.transformer.children():
             if isinstance(child, torch.nn.ModuleList):
@@ -565,11 +575,11 @@ class MPTForCausalLM(MPTPreTrainedModel):
 
         loss = None
         if labels is not None:
-            labels = torch.roll(labels, shifts=-1)
-            labels[:, -1] = -100
+            _labels = torch.roll(labels, shifts=-1)
+            _labels[:, -1] = -100
             loss = F.cross_entropy(
                 logits.view(-1, logits.size(-1)),
-                labels.to(logits.device).view(-1),
+                _labels.to(logits.device).view(-1),
             )
 
         return CausalLMOutputWithPast(

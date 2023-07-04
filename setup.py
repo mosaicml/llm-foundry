817c879123ef73292457e4d213f55556f9cfc080
Vitaliy Chiley <6439018+vchiley@users.noreply.github.com>
2023-07-03 20:34:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ extra_deps['peft'] = [
     'loralib==0.1.1',  # lora core
     # PyPI does not support direct dependencies, so we remove this line before uploading from PyPI
     # TODO: pin it down only after it stabilizes.
-    'peft @ git+https://github.com/huggingface/peft.git',
+    'peft@git+https://github.com/huggingface/peft.git',
     'bitsandbytes==0.39.1',  # 8bit
     'scipy>=1.10.0,<=1.11.0',  # bitsandbytes dependency; TODO: eliminate when incorporated to bitsandbytes
 ]

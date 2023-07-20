50331923282cf647adf384c6e7aa2a1fd5ae2496
Rishab Parthasarathy <rishab@mosaicml.com>
2023-07-20 16:10:49 -0700

    add to builders

--- llmfoundry/utils/builders.py
+++ llmfoundry/utils/builders.py
@@ -184,6 +184,8 @@ def build_icl_evaluators(icl_tasks,
                 ]
             elif icl_cfg.icl_task_type == 'question_answering':
                 icl_cfg.metric_names = ['InContextLearningQAAccuracy']
+            elif icl_cfg.icl_task_type == 'code_evaluation':
+                icl_cfg.metric_names = ['InContextLearningCodeEvalAccuracy']
             else:
                 raise ValueError(
                     f'No metric_names defined, unable to build default metrics for icl_task_type={icl_cfg.icl_task_type}.'

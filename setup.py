ad6545c112155e1d58eede42620a5e853d97d95b
Anna <anna@mosaicml.com>
2023-08-07 13:41:43 -0700

    Add MLFlow as a logger option (#475)

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ classifiers = [
 ]
 
 install_requires = [
-    'mosaicml[libcloud,nlp,wandb]>=0.15.0,<0.16',
+    'mosaicml[libcloud,nlp,wandb,mlflow]>=0.15.0,<0.16',
     'accelerate>=0.20,<0.21',  # for HF inference `device_map`
     'mosaicml-streaming>=0.5.1,<0.6',
     'torch>=1.13.1,<=2.0.1',

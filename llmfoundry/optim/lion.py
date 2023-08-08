65f77876badafe3bb5d51539cff4c5b79c9b18ad
Mihir Patel <mihir.v.patel7@gmail.com>
2023-08-07 23:07:30 -0700

    remove logging

--- llmfoundry/optim/lion.py
+++ llmfoundry/optim/lion.py
@@ -26,14 +26,6 @@ class DecoupledLionW(Optimizer):
         'l2_norm/grad':
             lambda param, optim_state, step_tensor: torch.linalg.vector_norm(
                 param.grad),
-        'cosine/update_grad':
-            lambda param, optim_state, step_tensor: torch.nn.functional.
-            cosine_similarity(
-                param.grad.flatten(), step_tensor.flatten(), dim=0),
-        'cosine/moment_grad':
-            lambda param, optim_state, step_tensor: torch.nn.functional.
-            cosine_similarity(
-                param.grad.flatten(), optim_state['exp_avg'].flatten(), dim=0),
     }
 
     def __init__(

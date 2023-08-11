f391b7fe2d1c641bdd2f943e78394a147330e85d
root <davis@mosaicml.com>
2023-08-11 20:59:08 +0000

    move fused error checks to llmfoundry

--- llmfoundry/optim/lion8b.py
+++ llmfoundry/optim/lion8b.py
@@ -301,6 +301,71 @@ def lion_step_unfused(grads: torch.Tensor,
     return momentums  # f32 upcast means not necessarily modified in place
 
 
+def lion8b_step_fused(grads: torch.Tensor,
+                      weights: torch.Tensor,
+                      momentums: torch.Tensor,
+                      scales: torch.Tensor,
+                      lr: float,
+                      beta1: float,
+                      beta2: float,
+                      weight_decay: float,
+                      errors: Optional[torch.Tensor] = None) -> None:
+    # just to save space in lists of allowed dtypes
+    f16, bf16, f32 = torch.float16, torch.bfloat16, torch.float32
+
+    use_errors = (errors is not None) and (weights.dtype in (f16, bf16))
+    orig_shape = weights.shape
+
+    # ------------------------------------------------ wall of error checking
+    quantize_group_size = 32
+    num_groups = (weights.numel() + quantize_group_size -
+                  1) // quantize_group_size
+    if (num_groups != scales.numel()):
+        raise ValueError(f'Expected {num_groups} quantization scales but ' +
+                         f' received {scales.numel()}')
+
+    for name, tensor, allowed_dtypes in [('grad', grads, (f16, bf16, f32)),
+                                         ('param', weights, (f16, bf16, f32)),
+                                         ('momentum', momentums, [torch.int8]),
+                                         ('scales', scales, [f16]),
+                                         ('errors', errors, [torch.uint8])]:
+        if name == 'errors' and not use_errors:
+            continue
+        if not tensor.is_cuda:
+            raise ValueError(
+                f'{name} must be on a CUDA device, not {tensor.device}')
+        if not tensor.is_contiguous():
+            raise ValueError(f'{name} is not contiguous!')
+        strides_unequal = tensor.stride() != weights.stride()
+        if name not in ('scales', 'errors') and strides_unequal:
+            raise ValueError(f'{name} stride {tensor.stride()} != ' +
+                             f'param stride {weights.stride()}')
+        if tensor.dtype not in allowed_dtypes:
+            raise ValueError(f'{name} must have dtype {allowed_dtypes}, not ' +
+                             f'{tensor.dtype}')
+        if (name != 'scales') and (orig_shape != tensor.shape):
+            raise ValueError(f'Param shape {orig_shape} != ' +
+                             f'{name} shape {tensor.shape}')
+
+    if grads.dtype in (torch.float16, torch.bfloat16):
+        allowed_dtypes = (grads.dtype, torch.float32)
+        if weights.dtype not in allowed_dtypes:
+            raise ValueError(
+                f'Weights must be f32 or match grad dtype {grads.dtype}')
+
+    # ------------------------------------------------ actual function call
+    from turbo import lion8b_step_cuda
+    return lion8b_step_cuda(grads=grads,
+                            weights=weights,
+                            momentums=momentums,
+                            scales=scales,
+                            lr=lr,
+                            beta1=beta1,
+                            beta2=beta2,
+                            weight_decay=weight_decay,
+                            errors=errors)
+
+
 def _lion8b_step(grads: torch.Tensor,
                  weights: torch.Tensor,
                  momentums: _MaybeQuantizedTensor,
@@ -312,8 +377,6 @@ def _lion8b_step(grads: torch.Tensor,
                  fused: bool = True) -> None:
 
     if momentums.is_quantized() and fused:
-        from turbo import lion8b_step as lion8b_step_fused
-
         assert momentums.quantized is not None  # pyright
         assert momentums.scales is not None  # pyright
         return lion8b_step_fused(grads=grads,

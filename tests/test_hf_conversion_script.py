add fix conversion script for other causal lms

tests/test_hf_conversion_script.py
@@ -44,21 +44,44 @@ def get_config(
     os.environ['TOKENIZERS_PARALLELISM'] = 'false'
     with open(conf_path) as f:
         test_cfg = om.load(f)
+
     return cast(DictConfig, test_cfg)
 
 
-@pytest.mark.parametrize('config_path', [
-    'scripts/train/yamls/pretrain/testing.yaml',
-    'scripts/train/yamls/pretrain/gpt-neo-125m.yaml'
-])
-def test_convert_and_generate_torch(config_path: str, tmp_path: pathlib.Path):
+@pytest.mark.parametrize('model', ['mpt', 'neo', 'llama2'])
+def test_convert_and_generate(model: str, tmp_path: pathlib.Path):
     delete_transformers_cache()
 
-    cfg = get_config(conf_path=config_path)
-    cfg['model']['init_device'] = 'cpu'
-    tokenizer = transformers.AutoTokenizer.from_pretrained(cfg.tokenizer.name)
-    model = COMPOSER_MODEL_REGISTRY[cfg['model'].name](cfg['model'], tokenizer)
-    trainer = Trainer(model=model)
+    om_cfg = None
+    if model == 'mpt':
+        om_cfg = get_config(
+            conf_path='scripts/train/yamls/pretrain/testing.yaml')
+    elif model == 'neo':
+        om_cfg = get_config(
+            conf_path='scripts/train/yamls/pretrain/gpt-neo-125m.yaml')
+        om_cfg['model']['config_overrides']['hidden_size'] = 36
+    elif model == 'llama2':
+        if 'HUGGING_FACE_HUB_TOKEN' not in os.environ:
+            pytest.skip(
+                'The CI cluster does not have access to the Llama models, so skip this test.'
+            )
+        om_cfg = get_config(
+            conf_path='scripts/train/yamls/pretrain/gpt-neo-125m.yaml')
+        om_cfg['model'][
+            'pretrained_model_name_or_path'] = 'meta-llama/Llama-2-7b-hf'
+        om_cfg['model']['config_overrides']['num_hidden_layers'] = 2
+        om_cfg['model']['use_auth_token'] = True
+        om_cfg['tokenizer']['name'] = 'meta-llama/Llama-2-7b-hf'
+    else:
+        raise ValueError(f'Unknown model {model}')
+    assert om_cfg is not None
+
+    om_cfg['model']['init_device'] = 'cpu'
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        om_cfg.tokenizer.name, use_auth_token=model == 'llama2')
+    original_model = COMPOSER_MODEL_REGISTRY[om_cfg['model'].name](
+        om_cfg['model'], tokenizer)
+    trainer = Trainer(model=original_model, device='cpu')
     trainer.save_checkpoint(os.path.join(tmp_path, 'checkpoint.pt'))
 
     args = Namespace(composer_path=os.path.join(tmp_path, 'checkpoint.pt'),
@@ -69,20 +92,29 @@ def test_convert_and_generate_torch(config_path: str, tmp_path: pathlib.Path):
                      test_uploaded_model=False)
     convert_composer_to_hf(args)
 
-    config = transformers.AutoConfig.from_pretrained(os.path.join(
-        tmp_path, 'hf-output-folder'),
-                                                     trust_remote_code=True)
-    model = transformers.AutoModelForCausalLM.from_pretrained(
+    loaded_config = transformers.AutoConfig.from_pretrained(
+        os.path.join(tmp_path, 'hf-output-folder'), trust_remote_code=True)
+    loaded_model = transformers.AutoModelForCausalLM.from_pretrained(
         os.path.join(tmp_path, 'hf-output-folder'),
-        config=config,
+        config=loaded_config,
         trust_remote_code=True)
     tokenizer = transformers.AutoTokenizer.from_pretrained(
         os.path.join(tmp_path, 'hf-output-folder'), trust_remote_code=True)
 
-    output = model.generate(tokenizer('hello',
-                                      return_tensors='pt')['input_ids'],
-                            max_new_tokens=1)
-    assert output.shape == (1, 2)
+    output = loaded_model.generate(tokenizer('hello',
+                                             return_tensors='pt')['input_ids'],
+                                   max_new_tokens=1)
+    assert output.shape == (1, 2 + (1 if model == 'llama2' else 0))
+
+    assert sum(p.numel() for p in original_model.model.parameters()) == sum(
+        p.numel() for p in loaded_model.parameters())
+    assert all(
+        str(type(module1)).split('.')[-1] == str(type(module2)).split('.')[-1]
+        for module1, module2 in zip(original_model.model.modules(),
+                                    loaded_model.modules()))
+    for p1, p2 in zip(original_model.model.parameters(),
+                      loaded_model.parameters()):
+        assert torch.allclose(p1, p2)
 
     delete_transformers_cache()
 
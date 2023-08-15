04a24b4f8901a50c92869cdb27d1dcfc84705207
Ying Chen <ying.chen@databricks.com>
2023-08-15 16:19:09 -0700

    nit

--- scripts/eval/eval.py
+++ scripts/eval/eval.py
@@ -122,8 +122,8 @@ def evaluate_model(model_cfg: DictConfig, cfg: DictConfig, run_name: str,
     assert isinstance(fsdp_config, Dict) or fsdp_config is None
 
     if fsdp_config and model_cfg.model.load_in_8bit:
-        raise ValueError("The FSDP config block is not supported when loading \
-                         Hugging Face models in 8bit.")
+        raise ValueError("The FSDP config block is not supported when loading " +
+                         "Hugging Face models in 8bit.")
 
     if hasattr(model_cfg.model, 'pretrained_lora_id_or_path'):
         composer_model = load_peft_model(model_cfg.model, tokenizer,

11663ba8a84aaba9994487387194814e7e642ceb
Chris Rinard <chris@mosaicml.com>
2023-08-22 12:10:45 -0700

    added some scripts

--- scripts/train/benchmarking/compile_settings.sh
+++ scripts/train/benchmarking/compile_settings.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-PROJECT="torch_compile_opts"
+PROJECT="torchcompile"
 TORCH_2_IMAGE="mosaicml/pytorch:2.0.1_cu118-python3.10-ubuntu20.04"
 CLUSTER_80GB=r1z1
 GIT_COMMIT=v0.2.0
 
-for FULLGRAPH in true
+for FULLGRAPH in false
 do
     for DYNAMIC in true false 
     do
         for MODE in default reduce-overhead max-autotune
         do 
-            python submit_benchmarks.py --project $PROJECT -m 125m.yaml -g 8 --microbatch_size 64 --accum 32 --image $TORCH_2_IMAGE --git_commit $GIT_COMMIT --gpu_type a100_80gb -t bf16 --cluster $CLUSTER_80GB -s 11 11 --RUN \
+            python submit_benchmarks.py --project $PROJECT -m 125m.yaml -g 8 --microbatch_size 60 --accum 32 --image $TORCH_2_IMAGE --git_commit $GIT_COMMIT --gpu_type a100_80gb -t bf16 --cluster $CLUSTER_80GB -s 11 11 --RUN \
                                         --torch_compile_fullgraph $FULLGRAPH --torch_compile_dynamic $DYNAMIC --torch_compile_mode $MODE
 
             python submit_benchmarks.py --project $PROJECT -m 760m.yaml -g 8 --microbatch_size 24 --accum  2 --image $TORCH_2_IMAGE --git_commit $GIT_COMMIT --gpu_type a100_80gb -t bf16 --cluster $CLUSTER_80GB -s 11 11 --RUN \

f4e30a0de34834a1420e57f916eaa1931764cb19
Chris Rinard <chris@mosaicml.com>
2023-08-23 09:41:47 -0700

    modify script

--- scripts/train/benchmarking/act_ckpt_optimize.sh
+++ scripts/train/benchmarking/act_ckpt_optimize.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-PROJECT="ackckpt"
+PROJECT="ackckptqt"
 TORCH_2_IMAGE="mosaicml/pytorch:2.0.1_cu118-python3.10-ubuntu20.04"
 CLUSTER_80GB=r1z1
-GIT_COMMIT=v0.2.0
+GIT_BRANCH=run_initial
 
-for MB_SIZE in 1 2 4 8
+for MB_SIZE in 1 2 4 6 8
 do
-    for GATH_LMT in true false
-    do
-        for CPU_OFFLOAD in true false
-        do
-            python submit_benchmarks.py --project $PROJECT -m 3b.yaml -g 8 --microbatch_size  $MB_SIZE --accum  2 --image $TORCH_2_IMAGE --git_commit $GIT_COMMIT --gpu_type a100_80gb -t bf16 --cluster $CLUSTER_80GB -s 11 11 --RUN --fsdp_config_limit_all_gathers $GATH_LMT --activation_cpu_offload $CPU_OFFLOAD --fsdp_config_activation_checkpointing false
-            python submit_benchmarks.py --project $PROJECT -m 7b.yaml -g 8 --microbatch_size  $MB_SIZE --accum 2 --image $TORCH_2_IMAGE --git_commit $GIT_COMMIT --gpu_type a100_80gb -t bf16 --cluster $CLUSTER_80GB -s 11 11 --RUN --fsdp_config_limit_all_gathers $GATH_LMT --activation_cpu_offload $CPU_OFFLOAD --fsdp_config_activation_checkpointing false
-            python submit_benchmarks.py --project $PROJECT -m 13b.yaml -g 8 --microbatch_size  $MB_SIZE --accum  2 --image $TORCH_2_IMAGE --git_commit $GIT_COMMIT --gpu_type a100_80gb -t bf16 --cluster $CLUSTER_80GB -s 11 11 --RUN --fsdp_config_limit_all_gathers $GATH_LMT --activation_cpu_offload $CPU_OFFLOAD --fsdp_config_activation_checkpointing false
-        done
-    done
+# #     for GATH_LMT in true false
+# #     do
+# #         for CPU_OFFLOAD in true false
+# #         do
+    python submit_benchmarks.py --project $PROJECT -m 3b.yaml -g 8 --microbatch_size  $MB_SIZE --accum  2 --image $TORCH_2_IMAGE --git_branch $GIT_BRANCH --gpu_type a100_40gb -t bf16 --cluster $CLUSTER_80GB -s 11 11 --RUN --fsdp_config_activation_checkpointing false
+    python submit_benchmarks.py --project $PROJECT -m 7b.yaml -g 8 --microbatch_size $MB_SIZE --accum 2 --image $TORCH_2_IMAGE --git_branch $GIT_BRANCH --gpu_type a100_80gb -t bf16 --cluster $CLUSTER_80GB -s 11 11 --RUN --fsdp_config_activation_checkpointing false
+    python submit_benchmarks.py --project $PROJECT -m 13b.yaml -g 8 --microbatch_size  $MB_SIZE --accum  2 --image $TORCH_2_IMAGE --git_branch $GIT_BRANCH --gpu_type a100_80gb -t bf16 --cluster $CLUSTER_80GB -s 11 11 --RUN --fsdp_config_activation_checkpointing false
+# #         done
+# #     done
 done
+
+# python submit_benchmarks.py --project $PROJECT -m 3b.yaml -g 8 --microbatch_size 12 --accum  2 --image $TORCH_2_IMAGE --git_branch $GIT_BRANCH --gpu_type a100_80gb -t bf16 --cluster $CLUSTER_80GB -s 11 11 --RUN --fsdp_config_limit_all_gathers $GATH_LMT --activation_cpu_offload $CPU_OFFLOAD --fsdp_config_activation_checkpointing false

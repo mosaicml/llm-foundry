0b2c6180de68a926aca4ad1df692944a7d504d70
Chris Rinard <chris@mosaicml.com>
2023-08-23 11:32:58 -0700

    reee

--- scripts/train/benchmarking/run_local.sh
+++ scripts/train/benchmarking/run_local.sh
@@ -7,4 +7,4 @@ IMAGE="mosaicml/pytorch:2.0.1_cu118-python3.10-ubuntu20.04"
 # A100 40GB
 CLUSTER_40GB=r1z1
 # seqlen 2048
-python submit_benchmarks.py --project $PROJECT -m 125m.yaml -g 8 --microbatch_size  24 --accum  3 --image $IMAGE --git_commit $GIT_COMMIT --gpu_type a100_40gb --cluster $CLUSTER_40GB -s 11 11 --RUN --LOCAL
+python submit_benchmarks.py --project $PROJECT -m 125m.yaml -g 8 --microbatch_size  24 --accum  3 --image $IMAGE --git_commit $GIT_COMMIT --gpu_type a100_80gb --cluster $CLUSTER_40GB -s 11 11 --RUN --LOCAL

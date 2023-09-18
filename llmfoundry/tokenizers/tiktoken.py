371185d59223949e420d915a2d281eed815e6604
Daniel King <daniel@mosaicml.com>
2023-09-17 18:18:32 -0700

    precommit

--- llmfoundry/tokenizers/tiktoken.py
+++ llmfoundry/tokenizers/tiktoken.py
@@ -106,8 +106,8 @@ class TiktokenTokenizerWrapper(PreTrainedTokenizer):
             self,
             ids: Union[int, List[int]],
             skip_special_tokens: bool = False) -> Union[str, List[str]]:
-        """Converts a single index or a sequence of indices in a token or a
-        
+        """Converts a single index or a sequence of indices in a token or a.
+
         sequence of tokens, using the vocabulary and added tokens.
 
         Args:

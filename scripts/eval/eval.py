0dc30b00cbfba84035c2a7eb779089cec654e2b9
Jeremy Dohmann <jeremy@mosaicml.com>
2023-09-19 20:20:42 -0400

    add rlhf prompts

--- scripts/eval/eval.py
+++ scripts/eval/eval.py
@@ -173,7 +173,7 @@ def evaluate_model(
         load_path=load_path,
         load_weights_only=True,
         progress_bar=False,
-        log_to_console=False,
+        log_to_console=True,
         dist_timeout=dist_timeout,
         python_log_level=python_log_level,
     )

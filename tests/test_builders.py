3d8a4b8647f7dde17046ac2d1e0f86905f9aeaa9
Daniel King <daniel@mosaicml.com>
2023-09-20 21:41:18 -0700

    precommit

--- tests/test_builders.py
+++ tests/test_builders.py
@@ -1,5 +1,7 @@
-import pytest
+# Copyright 2022 MosaicML LLM Foundry authors
+# SPDX-License-Identifier: Apache-2.0
 
+import pytest
 from transformers import PreTrainedTokenizerBase
 
 from llmfoundry.tokenizers.tiktoken import TiktokenTokenizerWrapper
@@ -7,11 +9,16 @@ from llmfoundry.utils.builders import build_tokenizer
 
 
 @pytest.mark.parametrize('tokenizer_name,tokenizer_kwargs', [
-    ('tiktoken', {'model_name': 'gpt-4'}),
-    ('EleutherAI/gpt-neo-125M', {'model_max_length': 10}),
-    ('mosaicml/mpt-7b', {'model_max_length': 20}),
-]
-)
+    ('tiktoken', {
+        'model_name': 'gpt-4'
+    }),
+    ('EleutherAI/gpt-neo-125M', {
+        'model_max_length': 10
+    }),
+    ('mosaicml/mpt-7b', {
+        'model_max_length': 20
+    }),
+])
 def test_tokenizer_builder(tokenizer_name: str, tokenizer_kwargs: dict):
     tokenizer = build_tokenizer(tokenizer_name, tokenizer_kwargs)
 
@@ -19,5 +26,6 @@ def test_tokenizer_builder(tokenizer_name: str, tokenizer_kwargs: dict):
         assert isinstance(tokenizer, TiktokenTokenizerWrapper)
         assert tokenizer.model_name == tokenizer_kwargs['model_name']
     else:
-        assert tokenizer.model_max_length == tokenizer_kwargs['model_max_length']
+        assert tokenizer.model_max_length == tokenizer_kwargs[
+            'model_max_length']
         assert isinstance(tokenizer, PreTrainedTokenizerBase)

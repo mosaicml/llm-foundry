c6f381bf81aeb1f1fd8560f1223f29f8eb7d9090
Daniel King <daniel@mosaicml.com>
2023-10-16 15:03:27 -0700

    precommit

--- llmfoundry/data/denoising.py
+++ llmfoundry/data/denoising.py
@@ -876,7 +876,8 @@ if __name__ == '__main__':
     tokenizer = build_tokenizer(tokenizer_name=tokenizer_name,
                                 tokenizer_kwargs=tokenizer_kwargs)
 
-    loader = build_text_denoising_dataloader(cfg, tokenizer, device_batch_size).dataloader
+    loader = build_text_denoising_dataloader(cfg, tokenizer,
+                                             device_batch_size).dataloader
     assert isinstance(loader.dataset, StreamingTextDataset)
 
     print(f'\n\nTRUNCATING TO: {loader.dataset.max_seq_len}\n\n')

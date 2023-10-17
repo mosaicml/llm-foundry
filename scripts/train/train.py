0cb3a4d658fe23a659a4cb039621df439dab6897
Chuck Tang <chuck@mosaicml.com>
2023-10-16 18:36:14 -0700

    ok

--- scripts/train/train.py
+++ scripts/train/train.py
@@ -463,7 +463,7 @@ def main(cfg: DictConfig) -> Trainer:
         profiler_schedule_cfg: Optional[Dict] = profiler_cfg.pop('schedule', None)
         if profiler_schedule_cfg:
             profiler_schedule = cyclic_schedule(**profiler_schedule_cfg)
-        # Only support json trace handler for now
+        # Only support json trace handler
         profiler_trace_handler: Optional[JSONTraceHandler] = None
         profiler_trace_cfg: Optional[Dict] = profiler_cfg.pop('json_trace_handler', None)
         if profiler_trace_cfg:

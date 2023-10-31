1eff648fb460943e31230eb7e2af813c0dc426a9
Shashank Rajput <shashank.rajput@databricks.com>
2023-10-31 17:25:57 +0000

    ..

--- llmfoundry/models/mpt/modeling_mpt.py
+++ llmfoundry/models/mpt/modeling_mpt.py
@@ -27,8 +27,9 @@ from composer.utils import dist
 from llmfoundry.models.layers.attention import is_flash_v2_installed
 
 if is_flash_v2_installed():
-    try: # This try...except is needed because transformers requires it despite the 'if' statement above
-        from flash_attn.layers.rotary import RotaryEmbedding as DAILRotaryEmbedding
+    try:  # This try...except is needed because transformers requires it despite the 'if' statement above
+        from flash_attn.layers.rotary import \
+            RotaryEmbedding as DAILRotaryEmbedding
     except Exception as e:
         raise e
 

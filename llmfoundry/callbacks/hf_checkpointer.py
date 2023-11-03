5872f3f464064bf3f77c66017a1d5c336a32c9b4
wenfeiy-db <wenfei.yan@databricks.com>
2023-11-02 18:12:39 -0700

    set task if the passed metadata does not include task

--- llmfoundry/callbacks/hf_checkpointer.py
+++ llmfoundry/callbacks/hf_checkpointer.py
@@ -74,12 +74,12 @@ class HuggingFaceCheckpointer(Callback):
         if self.mlflow_registered_model_name is not None:
             # Both the metadata and the task are needed in order for mlflow
             # and databricks optimized model serving to work
-            if 'metadata' not in mlflow_logging_config:
-                mlflow_logging_config['metadata'] = {
-                    'task': 'llm/v1/completions'
-                }
-            if 'task' not in mlflow_logging_config:
-                mlflow_logging_config['task'] = 'text-generation'
+            default_metadata = {
+                'task': 'llm/v1/completions'
+            }
+            passed_metadata = mlflow_logging_config.get('metadata', {})
+            mlflow_logging_config['metadata'] = {**default_metadata, **passed_metadata}
+            mlflow_logging_config.setdefault('task', 'text-generation')
         self.mlflow_logging_config = mlflow_logging_config
 
         self.huggingface_folder_name_fstr = os.path.join(

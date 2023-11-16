6791f3d00c96c431ffee3bebff17219bbbcad3ff
Daniel King <daniel@mosaicml.com>
2023-11-15 20:12:16 -0800

    fix typo

--- tests/test_mpt_gen.py
+++ tests/test_mpt_gen.py
@@ -147,7 +147,8 @@ def test_mpt_generate_callback(attn_impl: str, use_alibi: bool,
 @pytest.mark.parametrize('attn_impl', ['triton', 'torch'])
 @pytest.mark.parametrize('use_alibi', [True, False])
 def test_mpt_generate_callback_not_tied(
-        attn_impl: str, build_tiny_mpt: Callable[..., ComposerMPTCausalLM],
+        use_alibi: bool, attn_impl: str,
+        build_tiny_mpt: Callable[..., ComposerMPTCausalLM],
         tiny_ft_dataloader: DataLoader):
     device = get_device('gpu')
 
@@ -157,7 +158,7 @@ def test_mpt_generate_callback_not_tied(
         attn_config={
             'attn_impl': attn_impl,
             'attn_uses_sequence_id': False,
-            'alibi': True,
+            'alibi': use_alibi,
         },
     )
     model = device.module_to_device(model)

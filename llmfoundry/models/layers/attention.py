aa62d96a0a657fe759962ef6050ddce695222d6a
Shashank Rajput <shashank.rajput@databricks.com>
2023-12-21 07:59:41 +0000

    ..

--- llmfoundry/models/layers/attention.py
+++ llmfoundry/models/layers/attention.py
@@ -330,20 +330,6 @@ def flash_attn_fn(
             softmax_scale=softmax_scale,
             causal=reset_is_causal,
             return_attn_probs=needs_weights)
-    elif is_flash_v2_installed():
-        output_unpad = flash_attn_interface.flash_attn_varlen_func(
-            q=query_unpad,
-            k=key_unpad,
-            v=value_unpad,
-            cu_seqlens_q=cu_seqlens_q,
-            cu_seqlens_k=cu_seqlens_k,
-            max_seqlen_q=max_seqlen_q,
-            max_seqlen_k=max_seqlen_k,
-            dropout_p=dropout_p,
-            softmax_scale=softmax_scale,
-            causal=reset_is_causal,
-            return_attn_probs=needs_weights,
-            window_size=(sliding_window_size, sliding_window_size))
     elif is_flash_v2_installed(
             v2_version='2.3.6'
     ):  # TODO: Change to 2.3.7 and do not merge before 2.3.7 is released!
@@ -362,6 +348,20 @@ def flash_attn_fn(
             window_size=(sliding_window_size, sliding_window_size),
             alibi_slopes=attn_bias,
         )
+    elif is_flash_v2_installed():
+        output_unpad = flash_attn_interface.flash_attn_varlen_func(
+            q=query_unpad,
+            k=key_unpad,
+            v=value_unpad,
+            cu_seqlens_q=cu_seqlens_q,
+            cu_seqlens_k=cu_seqlens_k,
+            max_seqlen_q=max_seqlen_q,
+            max_seqlen_k=max_seqlen_k,
+            dropout_p=dropout_p,
+            softmax_scale=softmax_scale,
+            causal=reset_is_causal,
+            return_attn_probs=needs_weights,
+            window_size=(sliding_window_size, sliding_window_size))
     else:
         raise RuntimeError(
             'flash-attn==1.0.9 or flash-attn==2.3.6 is required.')

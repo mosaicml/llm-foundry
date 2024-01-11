9f7a866231f3e12046c702e6ed40eefc58fd8977
Irene Dea <deaairene@gmail.com>
2024-01-10 20:29:14 -0800

    wip

--- scripts/misc/download_hf_model.py
+++ scripts/misc/download_hf_model.py
@@ -5,13 +5,12 @@
 import argparse
 import logging
 import os
+import shutil
+import subprocess
+from typing import Dict
 
 import yaml
-import subprocess
-
 from huggingface_hub.constants import HUGGINGFACE_HUB_CACHE
-
-import shutil
 
 from llmfoundry.utils.model_download_utils import (download_from_cache_server,
                                                    download_from_hf_hub)
@@ -24,68 +23,66 @@ log = logging.getLogger(__name__)
 ORAS_PASSWD_PLACEHOLDER = '<placeholder_for_passwd>'
 ORAS_CLI = 'oras'
 
-def download_from_oras(model: str, save_dir: str, url: str, credentials_dirpath: str, model_to_path_map_file: str, concurrency: int):
-    """ Download from an OCI-compliant registry using oras."""
+
+def download_from_oras(model: str, save_dir: str, credentials_dirpath: str,
+                       config_file: str, concurrency: int):
+    """Download from an OCI-compliant registry using oras."""
     if shutil.which(ORAS_CLI) is None:
-        raise Exception(f'oras cli command `${ORAS_CLI}` is not found. Please install oras: https://oras.land/docs/installation ')
-    def validate_and_add_from_secret_file(secrets: str, secret_name: str, secret_file_path: str):
+        raise Exception(
+            f'oras cli command `{ORAS_CLI}` is not found. Please install oras: https://oras.land/docs/installation '
+        )
+
+    def validate_and_add_from_secret_file(secrets: Dict[str, str], secret_name: str,
+                                          secret_file_path: str):
         try:
-            with open(secret_file_path, encoding="utf-8") as f:
+            with open(secret_file_path, encoding='utf-8') as f:
                 secrets[secret_name] = f.read()
         except Exception as error:
-            raise ValueError(f'secret_file {secret_file_path} is failed to be read; but got error') from error
+            raise ValueError(
+                f'secret_file {secret_file_path} is failed to be read; but got error'
+            ) from error
 
     secrets = {}
-    validate_and_add_from_secret_file(secrets, 'username',
-                                      os.path.join(credentials_dirpath,
-                                                   'username'))
-    validate_and_add_from_secret_file(secrets, 'password',
-                                      os.path.join(credentials_dirpath,
-                                                   'password'))    
-    
-    with open(model_to_path_map_file, 'r', encoding='utf-8') as f:
-        model_to_path = yaml.safe_load(f.read())
-    
-    path = model_to_path[model]
-    
+    validate_and_add_from_secret_file(
+        secrets, 'username', os.path.join(credentials_dirpath, 'username'))
+    validate_and_add_from_secret_file(
+        secrets, 'password', os.path.join(credentials_dirpath, 'password'))
+
+    with open(config_file, 'r', encoding='utf-8') as f:
+        configs = yaml.safe_load(f.read())
+
+    path = configs['models'][model]
+    hostname = configs['hostname']
+
     def get_oras_cmd_to_run(password: str):
-        return [ORAS_CLI,
-                'pull',
-                '-o',
-                save_dir,
-                '--verbose',
-                '--concurrency',
-                concurrency,
-                '-u',
-                secrets['username'],
-                '-p',
-                password,
-                f'{url}/{path}'
-                ]
+        return [
+            ORAS_CLI, 'pull', '-o', save_dir, '--verbose', '--concurrency',
+            str(concurrency), '-u', secrets['username'], '-p', password,
+            f'{hostname}/{path}'
+        ]
 
     cmd_to_run = get_oras_cmd_to_run(ORAS_PASSWD_PLACEHOLDER)
     log.info(f'CMD for oras cli to run: {cmd_to_run}')
     cmd_to_run = get_oras_cmd_to_run(secrets['password'])
     subprocess.run(cmd_to_run, check=True)
 
-def parse_args()-> argparse.Namespace:
+
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
     # Add shared args
     parser.add_argument('--model', type=str, required=True)
     parser.add_argument('--download-from',
-                           type=str,
-                           choices=['hf', 'cache', 'oras'],
-                           default='hf')
-    parser.add_argument('--save-dir',
-                           type=str,
-                           default=HUGGINGFACE_HUB_CACHE)
-    
+                        type=str,
+                        choices=['hf', 'cache', 'oras'],
+                        default='hf')
+    parser.add_argument('--save-dir', type=str, default=HUGGINGFACE_HUB_CACHE)
+
     # Add HF args
     parser.add_argument('--token',
-                            type=str,
-                            default=os.getenv(HF_TOKEN_ENV_VAR))
-    
+                        type=str,
+                        default=os.getenv(HF_TOKEN_ENV_VAR))
+
     # Add cache args
     parser.add_argument('--cache-url', type=str, default=None)
     parser.add_argument('--ignore-cert', action='store_true', default=False)
@@ -94,14 +91,12 @@ def parse_args()-> argparse.Namespace:
         type=str,
         choices=['hf', 'oras', None],
         default=None,
-        help=
-        'Fallback target to download from if download from cache fails',
+        help='Fallback target to download from if download from cache fails',
     )
 
     # Add oras args
-    parser.add_argument('--oras-url', type=str, default=None)
-    parser.add_argument('--credentials_dirpath', type=str, default=None)
-    parser.add_argument('--model-to-path-map-file', type=str, default=None)
+    parser.add_argument('--credentials-dirpath', type=str, default=None)
+    parser.add_argument('--oras-config-file', type=str, default=None)
     parser.add_argument('--concurrency', type=int, default=10)
 
     return parser.parse_args()
@@ -111,14 +106,17 @@ if __name__ == '__main__':
     args = parse_args()
 
     if args.download_from != 'cache' and args.fallback is not None:
-        raise ValueError(f'Downloading from {args.download_from}, but fallback cannot be specified unless downloading from cache.')
+        raise ValueError(
+            f'Downloading from {args.download_from}, but fallback cannot be specified unless downloading from cache.'
+        )
 
     if args.download_from == 'hf':
         download_from_hf_hub(args.model,
                              save_dir=args.save_dir,
                              token=args.token)
     elif args.download_from == 'oras':
-        download_from_oras(args.model, args.save_dir, args.oras_url, args.credentials_dirpath, args.model_to_path_map_file, args.concurrency)
+        download_from_oras(args.model, args.save_dir, args.credentials_dirpath,
+                           args.oras_config_file, args.concurrency)
     else:
         try:
             download_from_cache_server(
@@ -139,8 +137,8 @@ if __name__ == '__main__':
                 old_level = logging.getLogger().level
                 logging.getLogger().setLevel(logging.ERROR)
                 download_from_hf_hub(args.model,
-                                    save_dir=args.save_dir,
-                                    token=args.token)
+                                     save_dir=args.save_dir,
+                                     token=args.token)
                 logging.getLogger().setLevel(old_level)
 
         except PermissionError:
@@ -151,11 +149,13 @@ if __name__ == '__main__':
             )
             if args.fallback == 'oras':
                 # save_dir, token, model, url, username, password, and config file
-                download_from_oras(args.model, args.save_dir, args.oras_url, args.credentials_dirpath, args.model_to_path_map_file, args.concurrency)
+                download_from_oras(args.model, args.save_dir,
+                                   args.credentials_dirpath,
+                                   args.oras_config_file, args.concurrency)
             elif args.fallback == 'hf':
                 # save_dir, token, model
                 download_from_hf_hub(args.model,
-                                    save_dir=args.save_dir,
-                                    token=args.token)
+                                     save_dir=args.save_dir,
+                                     token=args.token)
             else:
                 raise e

03113a9c61f2c47d3aac7478bf1a53299353d852
Shashank Rajput <shashank.rajput@databricks.com>
2024-01-17 21:30:42 +0000

    ..

--- llmfoundry/models/mpt/modeling_mpt.py
+++ llmfoundry/models/mpt/modeling_mpt.py
@@ -247,9 +247,11 @@ def gen_flash_attn_padding_info(
     _, indices_q, cu_seqlens_q, max_seqlen_q = unpadding_function(
         torch.zeros(bsz, S, 1).to(device=device), query_padding_mask)
     _, indices_k, cu_seqlens_k, max_seqlen_k = unpadding_function(
-        torch.zeros(bsz, S, 1).to(device=device), key_padding_mask)
+        torch.zeros(bsz, past_key_len + S, 1).to(device=device),
+        key_padding_mask)
     _, indices_v, _, _ = unpadding_function(
-        torch.zeros(bsz, S, 1).to(device=device), key_padding_mask)
+        torch.zeros(bsz, past_key_len + S, 1).to(device=device),
+        key_padding_mask)
 
     flash_attn_padding_info['indices_q'] = indices_q
     flash_attn_padding_info['indices_k'] = indices_k

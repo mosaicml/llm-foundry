b0acf3c5ac7031a301877bb13946545b732e0340
Daniel King <daniel@mosaicml.com>
2024-01-23 00:31:16 -0800

    temp

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ install_requires = [
     'accelerate>=0.25,<0.26',  # for HF inference `device_map`
     'transformers>=4.36,<4.37',
     'mosaicml-streaming>=0.7.2,<0.8',
-    'torch>=2.1,<2.1.1',
+    'torch>=2.1,<2.4', # TEMPORARY
     'datasets>=2.16,<2.17',
     'fsspec==2023.6.0',  # newer version results in a bug in datasets that duplicates data
     'sentencepiece==0.1.97',

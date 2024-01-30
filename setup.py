884d4b39e28840fb872d05c572b36560cf4354cd
Shashank Rajput <shashank.rajput@databricks.com>
2024-01-30 23:38:01 +0000

    ..

--- setup.py
+++ setup.py
@@ -99,10 +99,8 @@ extra_deps['tensorboard'] = [
 ]
 
 extra_deps['gpu'] = [
-    'flash-attn==1.0.9',
+    'flash-attn==2.4.2',
     'mosaicml-turbo==0.0.8',
-    # PyPI does not support direct dependencies, so we remove this line before uploading from PyPI
-    'xentropy-cuda-lib@git+https://github.com/HazyResearch/flash-attention.git@v1.0.9#subdirectory=csrc/xentropy',
 ]
 extra_deps['gpu-flash2'] = [
     'flash-attn==2.4.2',

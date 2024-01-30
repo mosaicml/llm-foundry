4bcbfc026fde0e184c2bc7bf51a11e11b2dbd9c1
Shashank Rajput <144760128+ShashankMosaicML@users.noreply.github.com>
2024-01-30 15:36:30 -0800

    Update llmfoundry/models/mpt/configuration_mpt.py
    Co-authored-by: Vitaliy Chiley <6439018+vchiley@users.noreply.github.com>

--- llmfoundry/models/mpt/configuration_mpt.py
+++ llmfoundry/models/mpt/configuration_mpt.py
@@ -227,7 +227,7 @@ class MPTConfig(PretrainedConfig):
         if self.attn_config['attn_impl'] == 'flash' and is_flash_v1_installed():
             warnings.warn(
                 DeprecationWarning(
-                    'The support for Flash Attention v1 will soon be deprecated. Please upgrade to Flash Attention v2.4.2.'
+                    'Support for Flash Attention v1 is deprecated. Please upgrade to Flash Attention v2.4.2.'
                 ))
 
         if self.attn_config[

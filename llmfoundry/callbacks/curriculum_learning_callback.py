09227a37fc10ad53b2f5e0c21e18aa106fa3df39
Saaketh <narayan.saaketh@gmail.com>
2024-02-02 16:09:35 -0800

    curriculum learning callback

---
+++ llmfoundry/callbacks/curriculum_learning_callback.py
@@ -0,0 +1,86 @@
+# Copyright 2022 MosaicML LLM Foundry authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""Resume from a checkpoint with a different dataset, enabling proto-curriculum-learning.
+
+This callback is currently experimental. The API may change in the future.
+"""
+
+import logging
+import json
+from typing import Any, Dict
+from composer.core import Callback
+from omegaconf import DictConfig, OmegaConf
+from streaming import StreamingDataset
+
+log = logging.getLogger(__name__)
+
+class CurriculumLearning(Callback):
+    """Starts a new epoch with a different dataset when resuming from a checkpoint.
+	
+	This callback is currently experimental. The API may change in the future.
+
+    Args:
+        dataset_index (int): The index of the dataset currently being used.
+		current_dataset_config (Dict):
+    """
+    def __init__(self, dataset_index: int, current_dataset_config: DictConfig):
+        self.dataset_index = dataset_index
+        self.saved_dataset_index = 0
+        self.all_dataset_configs = []
+        # The current dataset config is resolved and passed in train.py
+        self.current_dataset_config = OmegaConf.to_container(current_dataset_config)
+        self.current_dataset_state = {}
+        self.new_dataset_setup = False
+
+    def init(self, state, logger):
+        del logger
+
+        # If we are using a new dataset, we need to retrieve the dataset
+        # state before checkpoint load since it will be overwritten when
+        # the checkpoint is loaded. This contains important resumption
+        # information:  epoch, sample_in_epoch, num_canonical_nodes, etc.
+        dataset = self.state.train_dataloader.dataset
+        # Check if we are using a StreamingDataset
+        if not isinstance(self.state.train_dataloader.dataset, StreamingDataset):
+            raise ValueError(f"CurriculumLearning callback only supports StreamingDataset ",
+                             f"because it requires loading and saving dataset state. ",
+                             f"Instead, got a dataset of type {type(dataset)}")
+        # Save the current dataset state so we can restore it if needed.
+        self.current_dataset_state = dataset.state_dict(0, False)
+        log.info("Dataset state at init: ", self.current_dataset_state)
+        
+    def after_load(self, state, logger):
+        del logger
+
+		# As saved_dataset_index is loaded from state_dict, this only run when
+        # a user explicitly increments the dataset_index and not on any other
+		# resumption, including autoresume.
+        if self.saved_dataset_index < self.dataset_index:
+            dataset = self.state.train_dataloader.dataset
+			# Ignore the dataset state that was read in from the checkpoint, and
+            # replace with the new dataset state. This preserves resumption info.
+            dataset.load_state_dict(self.current_dataset_state)
+            # Start a new epoch since we are using a new dataset.
+            # This will also reset the sample_in_epoch written to checkpoint,
+            # making sure that subsequent resumptions proceed correctly.
+            state.timestamp.to_next_epoch()
+            self.new_dataset_setup = True
+        log.info("Dataset state after checkpoint load: ", dataset.state_dict(0, False))
+    
+    def state_dict(self):
+        if self.new_dataset_setup:
+            # Append the new dataset config to the list of all dataset configs.
+            self.all_dataset_configs.append(self.current_dataset_config)
+        log.info("CurriculumLearning callback dataset index: ", self.dataset_index)
+        log.info("CurriculumLearning callback all dataset configs: ", self.all_dataset_configs)
+        return {'dataset_index': self.dataset_index,
+                'all_dataset_configs': self.all_dataset_configs}
+    
+    def load_state_dict(self, state: Dict[str, Any]):
+        self.dataset_index = state['dataset_index']
+        self.all_dataset_configs = state['all_dataset_configs']
+        log.info("Datasets trained on with CurriculumLearning callback: ")
+        for i, dataset_config in enumerate(self.all_dataset_configs):
+            log.info(f"Dataset {i} config:")
+            log.info(dataset_config)
---
+++ llmfoundry/models/layers/rotary_embedding.py
@@ -0,0 +1,58 @@
+# Copyright 2022 MosaicML LLM Foundry authors
+# SPDX-License-Identifier: Apache-2.0
+
+# Code modified from https://github.com/huggingface/transformers/blob/main/src/transformers/models/llama/modeling_llama.py
+
+import torch
+from torch import nn
+
+
+class RotaryEmbedding(nn.Module):
+
+    def __init__(self, dim: int, max_position_embeddings: int, base: int,
+                 coefficient: float, shift: int,
+                 device: torch.device, dtype: torch.dtype):
+        super().__init__()
+
+        self.max_position_embeddings = max_position_embeddings
+
+        inv_freq = 1.0 / (coefficient*(base
+                          **(torch.arange(0+shift, dim+shift, 2).float().to(device) / dim)))
+        t = torch.arange(self.max_position_embeddings).to(inv_freq)
+
+        freqs = torch.einsum('i,j->ij', t, inv_freq)
+        # Different from paper, but it uses a different permutation in order to obtain the same calculation
+        emb = torch.cat((freqs, freqs), dim=-1)
+        self.cos_cached = emb.cos()[None, None, :, :].to(dtype)
+        self.sin_cached = emb.sin()[None, None, :, :].to(dtype)
+
+    def forward(self, dtype: torch.dtype, device: torch.device, seq_len: int):
+        # x: [bs, num_attention_heads, seq_len, head_size]
+        if seq_len > self.max_position_embeddings:
+            raise ValueError(
+                'The sequence length is greater than the maximum sequence length.'
+            )
+
+        return (
+            self.cos_cached[:, :, :seq_len, ...].to(dtype=dtype, device=device),
+            self.sin_cached[:, :, :seq_len, ...].to(dtype=dtype, device=device),
+        )
+
+
+def rotate_half(x: torch.Tensor):
+    """Rotates half the hidden dims of the input."""
+    x1 = x[..., :x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2:]
+    return torch.cat((-x2, x1), dim=-1)
+
+
+def apply_rotary_pos_emb(q: torch.Tensor, k: torch.Tensor, cos: torch.Tensor,
+                         sin: torch.Tensor, position_ids: torch.Tensor):
+    # The first two dimensions of cos and sin are always 1, so we can `squeeze` them.
+    cos = cos.squeeze(1).squeeze(0)  # [seq_len, dim]
+    sin = sin.squeeze(1).squeeze(0)  # [seq_len, dim]
+    cos = cos[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
+    sin = sin[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
+    q_embed = (q * cos) + (rotate_half(q) * sin)
+    k_embed = (k * cos) + (rotate_half(k) * sin)
+    return q_embed.to(q), k_embed.to(k)

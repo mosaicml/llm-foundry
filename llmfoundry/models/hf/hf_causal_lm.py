fa01bdacfba789fe323b84ec08c0da2e25b74423
Daniel King <daniel@mosaicml.com>
2024-02-08 16:36:40 -0800

    fix example yaml

--- llmfoundry/models/hf/hf_causal_lm.py
+++ llmfoundry/models/hf/hf_causal_lm.py
@@ -70,7 +70,7 @@ class ComposerHFCausalLM(HuggingFaceModelWithZLoss):
     def __init__(self, om_model_config: DictConfig,
                  tokenizer: PreTrainedTokenizerBase):
         pretrained_model_name_or_path = om_model_config.pretrained_model_name_or_path
-        lora_id_or_path = om_model_config.get('lora_id_or_path', None)
+        pretrained_lora_id_or_path = om_model_config.get('pretrained_lora_id_or_path', None)
 
         if not om_model_config.get(
                 'trust_remote_code', True
@@ -250,13 +250,13 @@ class ComposerHFCausalLM(HuggingFaceModelWithZLoss):
         if peft_config_dict is not None:
             peft_config = self._get_peft_config(peft_config_dict)
 
-        if lora_id_or_path is not None:
+        if pretrained_lora_id_or_path is not None:
             if not peft_installed:
                 raise ValueError(
                     'PEFT is not installed, but lora_id_or_path was passed. Please install LLM Foundry with the peft extra to use lora_id_or_path.'
                 )
             from peft import PeftModelForCausalLM
-            model = PeftModelForCausalLM.from_pretrained(model, lora_id_or_path)
+            model = PeftModelForCausalLM.from_pretrained(model, pretrained_lora_id_or_path)
 
         super().__init__(
             model=model,

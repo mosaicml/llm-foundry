9fa53c817ea219cf8589dd632bc410473e54d5a1
Saaketh <narayan.saaketh@gmail.com>
2024-02-11 14:43:20 -0800

    debugging

--- llmfoundry/models/layers/fc.py
+++ llmfoundry/models/layers/fc.py
@@ -13,9 +13,19 @@ try:
 except:
     pass
 
-try:
-    import float8_experimental
-    FC_CLASS_REGISTRY['fp8'] = float8_experimental.float8_linear.Float8Linear
-    FC_CLASS_REGISTRY['fp8dl'] = float8_experimental.float8_dynamic_linear.Float8DynamicLinear
-except:
-    pass
+# try:
+#     import float8_experimental
+#     print("import successful")
+#     FC_CLASS_REGISTRY['fp8'] = float8_experimental.float8_linear.Float8Linear
+#     print("fp8 successful")
+#     FC_CLASS_REGISTRY['fp8dl'] = float8_experimental.float8_dynamic_linear.Float8DynamicLinear
+#     print("fp8dl successful")
+# except:
+#     pass
+
+import float8_experimental
+print("import successful")
+FC_CLASS_REGISTRY['fp8'] = float8_experimental.float8_linear.Float8Linear
+print("fp8 successful")
+FC_CLASS_REGISTRY['fp8dl'] = float8_experimental.float8_dynamic_linear.Float8DynamicLinear
+print("fp8dl successful")

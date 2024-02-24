18d152d40f3cf630580678b4617136f0f8661305
Max Marion <max.marion@databricks.com>
2024-02-24 00:36:27 +0000

    wip

--- llmfoundry/utils/builders.py
+++ llmfoundry/utils/builders.py
@@ -466,6 +466,8 @@ def build_icl_evaluators(
                 icl_cfg.metric_names = ['InContextLearningQAAccuracy']
             elif icl_cfg.icl_task_type == 'code_evaluation':
                 icl_cfg.metric_names = ['InContextLearningCodeEvalAccuracy']
+            elif icl_cfg.icl_task_type == 'rag_generation':
+                icl_cfg.metric_names = ['poopoopeepee']
             else:
                 raise ValueError(
                     f'No metric_names defined, unable to build default metrics for icl_task_type={icl_cfg.icl_task_type}.'

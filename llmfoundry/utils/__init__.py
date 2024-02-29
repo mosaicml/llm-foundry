e202aaccf67e6477f8e6f759202402489971584a
Daniel King <daniel@mosaicml.com>
2024-02-29 01:34:02 -0800

    remove

--- llmfoundry/utils/__init__.py
+++ llmfoundry/utils/__init__.py
@@ -1,22 +1,17 @@
 # Copyright 2022 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
-try:
-    from llmfoundry.utils.builders import (build_algorithm, build_callback,
-                                           build_icl_evaluators, build_logger,
-                                           build_optimizer, build_scheduler,
-                                           build_tokenizer)
-    from llmfoundry.utils.checkpoint_conversion_helpers import (
-        convert_and_save_ft_weights, get_hf_tokenizer_from_composer_state_dict)
-    from llmfoundry.utils.config_utils import (calculate_batch_size_info,
-                                               log_config, pop_config,
-                                               update_batch_size_info)
-    from llmfoundry.utils.model_download_utils import (
-        download_from_hf_hub, download_from_http_fileserver)
-except ImportError as e:
-    raise ImportError(
-        'Please make sure to pip install . to get requirements for llm-foundry.'
-    ) from e
+from llmfoundry.utils.builders import (build_algorithm, build_callback,
+                                       build_icl_evaluators, build_logger,
+                                       build_optimizer, build_scheduler,
+                                       build_tokenizer)
+from llmfoundry.utils.checkpoint_conversion_helpers import (
+    convert_and_save_ft_weights, get_hf_tokenizer_from_composer_state_dict)
+from llmfoundry.utils.config_utils import (calculate_batch_size_info,
+                                           log_config, pop_config,
+                                           update_batch_size_info)
+from llmfoundry.utils.model_download_utils import (
+    download_from_hf_hub, download_from_http_fileserver)
 
 __all__ = [
     'build_callback',

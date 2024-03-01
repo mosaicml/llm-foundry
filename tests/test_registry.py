0afd15dd929b1fb4108c48e2bd050fac639af7cc
Daniel King <daniel@mosaicml.com>
2024-03-01 15:16:21 -0800

    add a test

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,4 +1,18 @@
 from llmfoundry.registry import import_file
+import os
+import pathlib
 
-def test_import_file():
-    
+def test_registry_init_code(tmp_path: pathlib.Path):
+    register_code = """
+import os
+os.environ['TEST_ENVIRON_REGISTRY_KEY'] = 'test'
+"""
+
+    with open(tmp_path / 'init_code.py', 'w') as _f:
+        _f.write(register_code)
+
+    import_file(tmp_path / 'init_code.py')
+
+    assert os.environ['TEST_ENVIRON_REGISTRY_KEY'] == 'test'
+
+    del os.environ['TEST_ENVIRON_REGISTRY_KEY']

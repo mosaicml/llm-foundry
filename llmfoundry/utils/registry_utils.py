d176ae1d3a40f51c1089403246398f460254249b
Daniel King <daniel@mosaicml.com>
2024-03-08 13:09:39 -0800

    pc

--- llmfoundry/utils/registry_utils.py
+++ llmfoundry/utils/registry_utils.py
@@ -1,3 +1,6 @@
+# Copyright 2024 MosaicML LLM Foundry authors
+# SPDX-License-Identifier: Apache-2.0
+
 import functools
 from typing import (Any, Callable, Dict, Generic, Optional, Sequence, Type,
                     TypeVar, Union)

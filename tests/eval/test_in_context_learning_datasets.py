08ef9089abad22b28578ed71f1746b7665844db0
Jeremy Dohmann <jeremy@mosaicml.com>
2024-03-11 13:10:08 -0400

    fix tet

--- tests/eval/test_in_context_learning_datasets.py
+++ tests/eval/test_in_context_learning_datasets.py
@@ -2046,15 +2046,13 @@ def test_eval_split_batch(mpt_tokenizer: transformers.AutoTokenizer,
         assert microbatch['generation_kwargs']['use_cache'] == True
         assert microbatch['generation_kwargs']['eos_token_id'] == 0
 
-
-@pytest.mark.parametrize('dataset_uri', ['lambada_small.jsonl'])
 @pytest.mark.parametrize('num_fewshot', [0, 5])
-@pytest.mark.gpu
-@pytest.mark.world_size(2)
-def test_lm_task_evaluation(dataset_uri: str, num_fewshot: int,
-                            tiny_gpt2_tokenizer: transformers.AutoTokenizer,
-                            tiny_gpt2_model: transformers.AutoModelForCausalLM,
-                            tmp_path: Path):
+@pytest.mark.parametrize('dataset_uri', ['lambada_small.jsonl'])
+# @pytest.mark.gpu
+# @pytest.mark.world_size(2)
+def test_lm_task_evaluation(num_fewshot: int, dataset_uri: str,
+        tiny_gpt2_tokenizer: transformers.AutoTokenizer, tmp_path: Path,
+        tiny_gpt2_model: transformers.AutoModelForCausalLM):
     pytest.importorskip('datasets')
     in_memory_logger = InMemoryLogger(
     )  # track the logged metrics in the in_memory_logger
@@ -2067,7 +2065,7 @@ def test_lm_task_evaluation(dataset_uri: str, num_fewshot: int,
         dataset_uri=dataset_uri,
         tokenizer=tokenizer,
         batch_size=batch_size,
-        max_seq_len=2048,
+        max_seq_len=1024,
         pad_tok_id=tokenizer.eos_token_id,
         num_fewshot=num_fewshot,
         prompt_string='',

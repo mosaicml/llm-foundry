5aa86bae09ce12314d1fe095f224b32ff2bdfa32
Daniel King <daniel@mosaicml.com>
2024-03-11 14:45:56 -0700

    docstring

--- llmfoundry/interfaces/callback_with_config.py
+++ llmfoundry/interfaces/callback_with_config.py
@@ -10,6 +10,10 @@ __all__ = ['CallbackWithConfig']
 
 
 class CallbackWithConfig(Callback, abc.ABC):
+    """A callback that takes a config dictionary as an argument, in addition to.
+
+    its other kwargs.
+    """
 
     def __init__(self, config: dict[str, Any], *args: Any,
                  **kwargs: Any) -> None:

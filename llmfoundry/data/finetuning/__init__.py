51f2eef41d8e76e277f557038816dc63f550b136
Xiaohan Zhang <xiaohan.zhang@databricks.com>
2024-03-14 14:51:52 -0700

    update

--- llmfoundry/data/finetuning/__init__.py
+++ llmfoundry/data/finetuning/__init__.py
@@ -3,6 +3,8 @@
 
 from llmfoundry.data.finetuning.collator import Seq2SeqFinetuningCollator
 from llmfoundry.data.finetuning.dataloader import build_finetuning_dataloader
-from llmfoundry.data.finetuning.tasks import _validate_chat_formatted_example, _validate_prompt_response_formatted_example
+from llmfoundry.data.finetuning.tasks import (_validate_chat_formatted_example,
+                                              _validate_prompt_response_formatted_example,
+                                              _get_example_type)
 
-__all__ = ['Seq2SeqFinetuningCollator', 'build_finetuning_dataloader', '_validate_chat_formatted_example', '_validate_prompt_response_formatted_example']
+__all__ = ['Seq2SeqFinetuningCollator', 'build_finetuning_dataloader', '_validate_chat_formatted_example', '_validate_prompt_response_formatted_example', '_get_example_type']

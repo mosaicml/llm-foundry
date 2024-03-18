ccfb4fabc0ead5fa08e445f017788ead4a811e7b
Angel Ruiz <angel.ruiz@databricks.com>
2024-03-18 11:36:56 -0700

    Revert "fix import"

--- scripts/eval/eval.py
+++ scripts/eval/eval.py
@@ -20,7 +20,8 @@ from omegaconf import OmegaConf as om
 from rich.traceback import install
 from transformers import PreTrainedTokenizerBase
 
-from llmfoundry.utils import find_mosaicml_logger, log_eval_analytics
+from llmfoundry.utils.mosaicmllogger_utils import (find_mosaicml_logger,
+                                                   log_eval_analytics)
 
 install()
 from llmfoundry.models.model_registry import COMPOSER_MODEL_REGISTRY
--- scripts/train/train.py
+++ scripts/train/train.py
@@ -23,7 +23,8 @@ from omegaconf import DictConfig, ListConfig
 from omegaconf import OmegaConf as om
 from rich.traceback import install
 
-from llmfoundry.utils import find_mosaicml_logger, log_train_analytics
+from llmfoundry.utils.mosaicmllogger_utils import (find_mosaicml_logger,
+                                                   log_train_analytics)
 
 install()
 

e0d9ff50320eafe1b89d5e268da2d8e636e0d104
Daniel King <daniel@mosaicml.com>
2024-03-18 22:43:14 -0700

    cleanup

--- llmfoundry/cli/registry_cli.py
+++ llmfoundry/cli/registry_cli.py
@@ -1,3 +1,6 @@
+# Copyright 2024 MosaicML LLM Foundry authors
+# SPDX-License-Identifier: Apache-2.0
+
 import typer
 from rich.console import Console
 from rich.table import Table
@@ -5,39 +8,66 @@ from rich.table import Table
 console = Console()
 from typing import Optional
 
-from llmfoundry.utils import registry_utils
 from llmfoundry import registry
+from llmfoundry.utils import registry_utils
 
-app = typer.Typer()
+app = typer.Typer(pretty_exceptions_show_locals=False)
+
+
+def _get_registries(
+        group: Optional[str] = None) -> list[registry_utils.TypedRegistry]:
+    registry_attr_names = dir(registry)
+    registry_attrs = [getattr(registry, name) for name in registry_attr_names]
+    available_registries = [
+        r for r in registry_attrs if isinstance(r, registry_utils.TypedRegistry)
+    ]
+
+    if group is not None and group not in registry_attr_names:
+        console.print(
+            f'Group {group} not found in registry. Run `llmfoundry registry get` to see available groups.'
+        )
+        return []
+
+    if group is not None:
+        available_registries = [getattr(registry, group)]
+
+    return available_registries
+
 
 @app.command()
 def get(group: Optional[str] = None):
-    registries = [
-        getattr(registry, name)
-        for name in (dir(registry) if group is None else [group])
-        if isinstance(getattr(registry, name), registry_utils.TypedRegistry)
-    ]
+    """Get the available registries.
 
-    options = [
-        r.get_all() for r in registries
-    ]
+    Args:
+        group (Optional[str], optional): The group to get. If not provided, all groups will be shown. Defaults to None.
+    """
+    available_registries = _get_registries(group)
 
-    table = Table("Registry", "Description", "Options")
-    for r, options in zip(registries, options):
-        table.add_row('.'.join(r.namespace), r.description, ", ".join(options))
+    table = Table('Registry', 'Description', 'Options', show_lines=True)
+    for r in available_registries:
+        table.add_row('.'.join(r.namespace), r.description,
+                      ', '.join(r.get_all()))
 
     console.print(table)
 
+
 @app.command()
 def find(group: str, name: str):
-    r = getattr(registry, group)
+    """Find a registry entry by name.
+
+    Args:
+        group (str): The group to search.
+        name (str): The name of the entry to search for.
+    """
+    available_registries = _get_registries(group)
+    if not available_registries:
+        return
+
+    r = available_registries[0]
     find_output = r.find(name)
 
-    table = Table(width=160)
-    table.add_column("Module")
-    table.add_column("File", overflow='fold', max_width=40)
-    table.add_column("Line number")
-    table.add_column("Docstring")
-    table.add_row(find_output['module'], find_output['file'], str(find_output['line_no']), find_output['docstring'])
+    table = Table('Module', 'File', 'Line number', 'Docstring')
+    table.add_row(find_output['module'], find_output['file'],
+                  str(find_output['line_no']), find_output['docstring'])
 
     console.print(table)

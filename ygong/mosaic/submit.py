9acc479f2759dd27669a3588b391e90f2bb7de01
Yu Gong <129110170+ygong1@users.noreply.github.com>
2024-03-22 10:32:37 -0700

    aa

--- ygong/mosaic/submit.py
+++ ygong/mosaic/submit.py
@@ -17,6 +17,7 @@ from databricks.sdk import WorkspaceClient
 # from databricks_genai.api.config import configure_request
 from mcli import config
 from mcli.api.runs.api_get_runs import get_run
+import logging
     
         
 def _set_up_environment(content: str):
@@ -125,16 +126,21 @@ def _display_run_summary(summary: pd.DataFrame, cancel_button: Optional[widgets.
         display(cancel_button)
     display(HTML(summary.to_html(escape=False)))
 
-def _wait_for_run_status(run: Run, status: RunStatus):
+def _wait_for_run_status(run: Run, status: RunStatus, inclusive: bool = True):
     run_name = run.name
-    while not run.status.after(status, inclusive=True):
+    while not run.status.after(status, inclusive=inclusive):
         time.sleep(5)
-        run =  get_run(run_name)    
+        run =  get_run(run_name)
+        logging.debug(f"DEBUG: run status {run.status}, expected status {status}")
+    logging.debug(f"DEBUG: finish waiting run reached expected status {status}")
     return run
 
 
 
-def submit(model, config: any, scalingConfig: ScalingConfig, sync: bool = False):
+def submit(model, config: any, scalingConfig: ScalingConfig, sync: bool = False, debug: bool = False):
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+
     _init_connection()
     mlflow_experiment_name = None
     if model == "mpt125m":
@@ -154,6 +160,7 @@ def submit(model, config: any, scalingConfig: ScalingConfig, sync: bool = False)
         clear_output(wait=False)
         run = get_run(run_name)
         run.stop()
+        logging.debug(f"DEBUG: run {run_name} is cancelled")
         run = _wait_for_run_status(run, RunStatus.TERMINATING)
         summary = _get_run_summary(run, mlflow_experiment_name)
         display(HTML(summary.to_html(escape=False)))
@@ -173,15 +180,19 @@ def submit(model, config: any, scalingConfig: ScalingConfig, sync: bool = False)
         try:
             run = get_run(run)
             if run.status.after(RunStatus.TERMINATING, inclusive=True):
+                logging.debug(f"DEBUG: run {run_name} is terminated. Status {run.status}")
                 break
             summary = _get_run_summary(run, mlflow_experiment_name)
             _display_run_summary(summary, button)
             break
         except ValueError:
-             print(f"DEBUG: waiting for the MLFLow experiment run to be ready, run status{run.status}")
+             
+             logging.debug(f"DEBUG: waiting for the MLFLow experiment run to be ready, run status{run.status}")
              pass
 
     if sync:
-        print(f"DEBUG: synchronously waiting for the run to finish.")
-        run = _wait_for_run_status(run, RunStatus.TERMINATING)
+        logging.debug(f"DEBUG: synchronously waiting for the run to finish.")
+        run = _wait_for_run_status(run, RunStatus.TERMINATING, inclusive=False)
         _display_run_summary(_get_run_summary(run, mlflow_experiment_name), None)
+    
+    return run

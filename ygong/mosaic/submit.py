1359e58f319587a725a7f49217c56bebd246c2b4
Yu Gong <129110170+ygong1@users.noreply.github.com>
2024-03-27 10:39:21 -0700

    make it works in job workflow

--- ygong/mosaic/submit.py
+++ ygong/mosaic/submit.py
@@ -17,7 +17,11 @@ from databricks.sdk import WorkspaceClient
 # from databricks_genai.api.config import configure_request
 from mcli import config
 from mcli.api.runs.api_get_runs import get_run
-    
+import logging
+import sys
+
+logger = logging.getLogger('ygong.mosaic.submit')
+
         
 def _set_up_environment(content: str):
     os.environ['CREDENTIALS'] = content
@@ -33,6 +37,7 @@ def _init_connection():
             return True
         
      if _is_local():
+        logger.debug("init_connection in local mode")
         if os.environ.get('CREDENTIALS') is None:
             raise ValueError("_set_up_environment must be manually called to configure credentials for local runs")
         data = json.loads(base64.b64decode(os.environ.get('CREDENTIALS')).decode('utf-8'))
@@ -68,23 +73,28 @@ def _init_connection():
         create_secret(s)
 
      else:
+        logger.debug("init_connection in databricks environment")
         wc = WorkspaceClient()
-        import mlflow.utils.databricks_utils as databricks_utils
-        workspace_url = databricks_utils.get_workspace_info_from_dbutils()[0]
         ctx = wc.dbutils.entry_point.getDbutils().notebook().getContext()
         token = ctx.apiToken().get()
         api_url = ctx.apiUrl().get()
         endpoint = f'{api_url}/api/2.0/genai-mapi/graphql'
+        workspace_url = api_url
         os.environ[config.MOSAICML_API_KEY_ENV] = f'Bearer {token}'
         os.environ[config.MOSAICML_API_ENDPOINT_ENV] = endpoint
+        try:
+            jobs_id = ctx.jobId().get()
+            os.environ['JOB_ID'] = jobs_id
+        except:
+            pass
 
      # needed to set up the MLFlow query for experiment runs   
      os.environ['WORKSPACE_URL'] = workspace_url
      os.environ['MLFLOW_TRACKING_TOKEN'] = token
+     logger.debug(f"init_connection token: {os.environ['MLFLOW_TRACKING_TOKEN']}, workspace: {os.environ['WORKSPACE_URL']}, is_jobs: {os.environ.get('JOB_ID')}")
+        
      
      
-     
-
 def get_experiment_run_url(tracking_uri: Optional[str], experiment_name: str, run_name: str):
       if tracking_uri is None:
           raise ValueError("tracking_uri must be provided")
@@ -109,7 +119,6 @@ def _get_run_summary(run: Run, experiment_name: Optional[str] = None):
     url = None
     if run.status == RunStatus.RUNNING and experiment_name is not None:
           url = get_experiment_run_url(os.environ.get('WORKSPACE_URL'), experiment_name, run.name)
-    print(f"ygong: url {url}")
     
     df = pd.DataFrame({
          'Run Name': [run.name],
@@ -125,16 +134,30 @@ def _display_run_summary(summary: pd.DataFrame, cancel_button: Optional[widgets.
         display(cancel_button)
     display(HTML(summary.to_html(escape=False)))
 
-def _wait_for_run_status(run: Run, status: RunStatus):
+def _wait_for_run_status(run: Run, status: RunStatus, inclusive: bool = True):
     run_name = run.name
-    while not run.status.after(status, inclusive=True):
+    while not run.status.after(status, inclusive=inclusive):
         time.sleep(5)
-        run =  get_run(run_name)    
+        run =  get_run(run_name)
+        logger.debug(f"run status {run.status}, expected status {status}")
+    logger.debug(f"finish waiting run reached expected status {status}")
     return run
 
 
 
-def submit(model, config: any, scalingConfig: ScalingConfig, sync: bool = False):
+def submit(model, config: any, scalingConfig: ScalingConfig, sync: bool = False, debug: bool = False):
+    if debug:
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setLevel(logging.DEBUG)  # Set minimum log level for the handler
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        stdout_handler.setFormatter(formatter)
+
+        # Add the handler to the logger
+        logger.addHandler(stdout_handler)
+        logger.setLevel(logging.DEBUG)
+        
+        logger.info("set the logger to debug mode")
+
     _init_connection()
     mlflow_experiment_name = None
     if model == "mpt125m":
@@ -149,17 +172,22 @@ def submit(model, config: any, scalingConfig: ScalingConfig, sync: bool = False)
     run = create_run(runConfig)
     run_name = run.name
     # Create a button
-    button = widgets.Button(description="cancel the run")
-    def on_button_clicked(b):
-        clear_output(wait=False)
-        run = get_run(run_name)
-        run.stop()
-        run = _wait_for_run_status(run, RunStatus.TERMINATING)
-        summary = _get_run_summary(run, mlflow_experiment_name)
-        display(HTML(summary.to_html(escape=False)))
-    button.on_click(on_button_clicked)
+    if os.environ.get('JOB_ID') is not None:
+        # running in jobs workflow, no need to cancel the run and doesn't support widgets
+        button = None
+    else:
+        button = widgets.Button(description="cancel the run")
+        def on_button_clicked(b):
+            clear_output(wait=False)
+            run = get_run(run_name)
+            run.stop()
+            logger.debug(f"run {run_name} is cancelled")
+            run = _wait_for_run_status(run, RunStatus.TERMINATING)
+            summary = _get_run_summary(run, mlflow_experiment_name)
+            display(HTML(summary.to_html(escape=False)))
+        button.on_click(on_button_clicked)
     _display_run_summary(_get_run_summary(run, mlflow_experiment_name), button)
-
+   
     # setting mlflow_experiment_name to be None, since its very likely mlflow run isn't ready yet
     # when the run just starts running
     run = _wait_for_run_status(run, RunStatus.RUNNING)
@@ -173,15 +201,19 @@ def submit(model, config: any, scalingConfig: ScalingConfig, sync: bool = False)
         try:
             run = get_run(run)
             if run.status.after(RunStatus.TERMINATING, inclusive=True):
+                logger.debug(f"run {run_name} is terminated. Status {run.status}")
                 break
             summary = _get_run_summary(run, mlflow_experiment_name)
             _display_run_summary(summary, button)
             break
         except ValueError:
-             print(f"DEBUG: waiting for the MLFLow experiment run to be ready, run status{run.status}")
+             
+             logger.debug(f"waiting for the MLFLow experiment run to be ready, run status{run.status}")
              pass
 
     if sync:
-        print(f"DEBUG: synchronously waiting for the run to finish.")
-        run = _wait_for_run_status(run, RunStatus.TERMINATING)
+        logger.debug(f"synchronously waiting for the run to finish.")
+        run = _wait_for_run_status(run, RunStatus.TERMINATING, inclusive=False)
         _display_run_summary(_get_run_summary(run, mlflow_experiment_name), None)
+    
+    return run

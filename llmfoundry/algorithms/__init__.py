bfa9a3692129bd2e307f0cf2902d10e8cb2d629d
Joqsan Azocar <joqsan.a@gmail.com>
2024-04-01 12:01:23 +0000

    add SAM as Composer algorithm

--- llmfoundry/algorithms/__init__.py
+++ llmfoundry/algorithms/__init__.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from composer.algorithms import (Alibi, GatedLinearUnits, GradientClipping,
-                                 LowPrecisionLayerNorm)
+                                 LowPrecisionLayerNorm, SAM)
 
 from llmfoundry.registry import algorithms
 
@@ -10,3 +10,4 @@ algorithms.register('gradient_clipping', func=GradientClipping)
 algorithms.register('alibi', func=Alibi)
 algorithms.register('gated_linear_units', func=GatedLinearUnits)
 algorithms.register('low_precision_layernorm', func=LowPrecisionLayerNorm)
+algorithms.register('sam', func=SAM)

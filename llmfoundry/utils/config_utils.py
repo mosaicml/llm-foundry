bbcabcc5634fbbc93d414515a42c32c8ffdff377
Vincent Chen <v.chen@databricks.com>
2024-04-18 17:15:11 -0700

    precommit

--- llmfoundry/utils/config_utils.py
+++ llmfoundry/utils/config_utils.py
@@ -189,14 +189,7 @@ def log_config(cfg: DictConfig) -> None:
 
 
 def parse_source_dataset(cfg: DictConfig):
-    """Parse a run config for dataset information related to training and
-    evaluation stages.
-
-    It supports extracting paths from different sources including local
-    filesystem, remote locations, Hugging Face datasets, Delta tables, and UC
-    volume paths. The function aggregates unique dataset identifiers and their
-    types from the configuration.
-    """
+    """Parse a run config for dataset information."""
     data_paths = set()
 
     for data_split in ['train', 'eval']:
@@ -242,15 +235,7 @@ def parse_source_dataset(cfg: DictConfig):
 
 
 def log_dataset_uri(cfg: DictConfig) -> mlflow.data.meta_dataset.MetaDataset:
-    """Extracts dataset information from the provided configuration and
-    translates it into MLFlow-compatible dataset source instances.
-
-    Args:
-        cfg (DictConfig): The run configuration object containing dataset definitions.
-
-    Returns:
-        List[mlflow.data.meta_dataset.MetaDataset]: A list of MetaDataset instances,
-    """
+    """Extracts dataset information from the provided configuration."""
     # Figure out which data source to use
     data_paths = parse_source_dataset(cfg)
 

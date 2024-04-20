342641511f6d4b1f26aadea9939c619bf92b4bfc
Daniel King <43149077+dakinggg@users.noreply.github.com>
2024-04-19 17:32:56 -0700

    Pin mlflow (#1124)

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ classifiers = [
 
 install_requires = [
     'mosaicml[libcloud,wandb,oci,gcs]>=0.21.3,<0.22',
-    'mlflow>=2.10,<3',
+    'mlflow>=2.10,<2.12',
     'accelerate>=0.25,<0.26',  # for HF inference `device_map`
     'transformers>=4.40,<4.41',
     'mosaicml-streaming>=0.7.5,<0.8',

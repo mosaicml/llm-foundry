e309366b2e0b2ed7fd1aef065e8cea5d460b5684
Milo Cress <milo.cress@databricks.com>
2024-04-22 18:17:12 +0000

    import TrainConfig just in case?

--- scripts/inference/convert_composer_to_hf.py
+++ scripts/inference/convert_composer_to_hf.py
@@ -19,6 +19,8 @@ from llmfoundry.utils import get_hf_tokenizer_from_composer_state_dict
 from llmfoundry.utils.checkpoint_conversion_helpers import load_tokenizer
 from llmfoundry.utils.huggingface_hub_utils import \
     edit_files_for_hf_compatibility
+from scripts.eval.eval import EvalConfig
+from scripts.train.train import TrainConfig
 
 
 def write_huggingface_pretrained_from_composer_checkpoint(

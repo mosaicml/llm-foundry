d4810e6cd3a672fd40a04bb76cdf43be4887517c
Irene Dea <deaairene@gmail.com>
2024-04-24 18:02:25 -0700

    wip

--- llmfoundry/data/finetuning/tasks.py
+++ llmfoundry/data/finetuning/tasks.py
@@ -451,6 +451,7 @@ def _force_timeout_subprocess(timeout: int):
     def sleep_and_timeout(timeout: int):
         time.sleep(timeout)
         os.kill(os.getpid(), signal.SIGTERM)
+        raise TimeoutError(f'Timed out after {timeout} seconds')  
     
     timeout_process = SpawnProcess(target=sleep_and_timeout, args=(timeout,))
     timeout_process.start()

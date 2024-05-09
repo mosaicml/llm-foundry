4541f38de30cf18a7f94c034e1c97d324589c5dc
Chuck Tang <chuck.tang@databricks.com>
2024-05-08 18:23:26 -0700

    commit change

--- setup.py
+++ setup.py
@@ -48,9 +48,9 @@ while True:
 
 classifiers = [
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
 ]
 
 install_requires = [

2f3653313b12ca3405d8e541f11877073172912d
Milo Cress <milo.cress@databricks.com>
2024-05-14 19:05:00 +0000

    fix?

--- scripts/pyhooks_stub.py
+++ scripts/pyhooks_stub.py
@@ -1,13 +1,26 @@
+# Copyright 2024 MosaicML LLM Foundry authors
+# SPDX-License-Identifier: Apache-2.0
+
 import subprocess
 import sys
+
 # execute some shell commands
 
 # git clone git@github.com:databricks-mosaic/mcloud.git to /tmp/mcloud
 # git checkout pyhookbuffered
-subprocess.run(["git", "clone", "git@github.com:databricks-mosaic/mcloud.git", "/tmp/mcloud"])
-subprocess.run(["git", "checkout", "pyhookbuffered"], cwd="/tmp/mcloud")
-
+try:
+    subprocess.run([
+        'git',
+        'clone',
+        'git@github.com:databricks-mosaic/mcloud.git',
+        '/tmp/mcloud',
+    ])
+    subprocess.run(['git', 'checkout', 'pyhookbuffered'], cwd='/tmp/mcloud')
+except Exception as e:
+    print(
+        f'Failed to clone mcloud: {e}, probably because it already exists, no big deal.',
+    )
 
 # add the following to the PYTHONPATH:
 # /tmp/mcloud/finetuning/pyhook
-sys.path.append("/tmp/mcloud/finetuning/pyhook")
+sys.path.append('/tmp/mcloud/finetuning/pyhook')

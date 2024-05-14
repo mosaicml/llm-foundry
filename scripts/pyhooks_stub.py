9c44f13211904e400bd43dfc048fd30c46ed8320
Milo Cress <milo.cress@databricks.com>
2024-05-14 20:21:20 +0000

    rank

--- scripts/pyhooks_stub.py
+++ scripts/pyhooks_stub.py
@@ -1,32 +1,32 @@
 # Copyright 2024 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
+# git clone git@github.com:databricks-mosaic/mcloud.git to /tmp/mcloud
+# git checkout pyhookbuffered
+import os
 import subprocess
 import sys
 
 # execute some shell commands
 
-# git clone git@github.com:databricks-mosaic/mcloud.git to /tmp/mcloud
-# git checkout pyhookbuffered
-try:
-    subprocess.run([
-        'git',
-        'clone',
-        'git@github.com:databricks-mosaic/mcloud.git',
-        '/tmp/mcloud',
-    ],
-                   check=True)
-    subprocess.run(['git', 'checkout', 'pyhookbuffered'],
-                   cwd='/tmp/mcloud',
-                   check=True)
-except Exception as e:
-    print(
-        f'Failed to clone mcloud: {e}, probably because it already exists, no big deal.',
-    )
+rank = os.environ.get('RANK')
+if rank is None:
+    raise ValueError('RANK not set')
+
+subprocess.run([
+    'git',
+    'clone',
+    'git@github.com:databricks-mosaic/mcloud.git',
+    f'/tmp/mcloud-{rank}',
+],
+               check=True)
+subprocess.run(['git', 'checkout', 'pyhookbuffered'],
+               cwd=f'/tmp/mcloud-{rank}',
+               check=True)
 
 # add the following to the PYTHONPATH:
 # /tmp/mcloud/finetuning/pyhook
-sys.path.append('/tmp/mcloud/finetuning/')
+sys.path.append(f'/tmp/mcloud-{rank}/finetuning/')
 
 try:
     import pyhook

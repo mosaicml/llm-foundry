b5bc9e12fc8d89c24c8155e1dcb024e71d0feb89
moeiniamir <amirmoini78@gmail.com>
2024-05-15 09:00:52 +0200

    gptq.py: add no_save and debug

--- llmfoundry/utils/gptq.py
+++ llmfoundry/utils/gptq.py
@@ -4,6 +4,9 @@ import omegaconf
 from pathlib import Path
 from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
 from composer.utils import reproducibility
+import torch
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "5"
 
 
 def get_calibration_data(cfg: omegaconf.DictConfig):
@@ -53,7 +56,7 @@ def get_custom_calibration_data(cfg: omegaconf.DictConfig):
     return examples
 
 # %%
-def quantize_and_save(cfg: omegaconf.DictConfig, examples, path):
+def quantize_and_save(cfg: omegaconf.DictConfig, examples, path, **kwargs):
     quantize_config = BaseQuantizeConfig(
         bits=cfg.wbits,
         group_size=cfg.gs,
@@ -65,17 +68,18 @@ def quantize_and_save(cfg: omegaconf.DictConfig, examples, path):
     model = AutoGPTQForCausalLM.from_pretrained(
         cfg.model_id,
         quantize_config,
-        device_map="auto")
+        **kwargs)
     model.quantize(examples)
 
     if cfg.get('no_save', False):
         return model
     print(f"Saving gptq model to {path}")
-    model.save_pretrained(path)
+    model.save_quantized(path)
 
     import gc
     del model
     gc.collect()
+    return None
 
 
 def get_llama_marlin_factory(cfg, original_from_pretrained=None):
@@ -85,31 +89,39 @@ def get_llama_marlin_factory(cfg, original_from_pretrained=None):
             transformers.AutoModelForCausalLM.from_pretrained = original_from_pretrained
         from auto_gptq import AutoGPTQForCausalLM
 
-        gptq_save_dir = Path.home() / "saved" / \
-            f"{cfg.model_id.split('/')[-1]}-gptq{cfg.wbits}-{cfg.gs}-{cfg.actorder}"
-        print(f"Loading gptq model from {gptq_save_dir}")
-        if cfg.get('suffix'):
-            gptq_save_dir = gptq_save_dir.with_name(
-                gptq_save_dir.name + f"-{cfg.get('suffix')}")
+        gptq_save_dir = cfg.get('gptq_save_dir')
+        if gptq_save_dir:
+            gptq_save_dir = Path(gptq_save_dir)
+        else:
+            gptq_save_dir = Path.home() / "saved" / \
+                f"{cfg.model_id.split('/')[-1]}-gptq{cfg.wbits}-{cfg.gs}-{cfg.actorder}"
+            if cfg.get('suffix'):
+                gptq_save_dir = gptq_save_dir.with_name(
+                    gptq_save_dir.name + f"-{cfg.get('suffix')}")
 
         # check if checkpoint exists as a folder
         if not gptq_save_dir.is_dir() or cfg.get('no_save', False):
-            print('checkpoint not found, creating...')
+            if cfg.get('no_save', False):
+                assert not cfg.get('use_marlin', False), "use_marlin=True is not supported with no_save=True"
+            print(f'{gptq_save_dir} not found, creating...')
             print(cfg)
             if cfg.get('chunked', False):
                 calibration_data = get_custom_calibration_data(cfg)
             else:
                 calibration_data = get_calibration_data(cfg)
-            model = quantize_and_save(cfg, calibration_data, gptq_save_dir)
+            model = quantize_and_save(cfg, calibration_data, gptq_save_dir, **kwargs)
+            if cfg.get('no_save', False):
+                return model
 
+        print(f"Loading gptq model from {gptq_save_dir}")
         model = AutoGPTQForCausalLM.from_quantized(
-            gptq_save_dir, use_marlin=cfg.get('use_marlin', False))
+            gptq_save_dir, use_marlin=cfg.get('use_marlin', False), **kwargs)
 
         return model
     return get_llama_marlin
 
 
-if __name__ == "__main__":
+if __name__ == "!__main__":
     from omegaconf import OmegaConf
     cfg = OmegaConf.from_cli()
     cfg_default = OmegaConf.create(
@@ -117,14 +129,28 @@ if __name__ == "__main__":
       model_id: meta-llama/Meta-Llama-3-8B-Instruct
       wbits: 4
       gs: 128
-      actorder: False
+      actorder: True
+    #   no_save: True
+    #   clip: True
       seed: 1
-      no_save: True
-      clip: True
+      suffix: seed${seed}-clip
+    #   dataset: /nfs/scistore19/alistgrp/amoeini/group_10_merged.txt
+    #   chunked: true
+    #   chunk_size: 1024      
         """
     )
     cfg = OmegaConf.merge(cfg_default, cfg)
     if cfg.get('seed'):
         cfg.suffix = f"seed{cfg.seed}"
         reproducibility.seed_all(cfg.seed)
-    get_llama_marlin_factory(cfg)()
+    import os 
+    if os.environ.get("DEBUG"):
+        import logging
+        logging.basicConfig(level=logging.DEBUG)
+    model = get_llama_marlin_factory(cfg)()
+    
+if __name__ == "__main__":
+    from transformers import AutoModelForCausalLM, GPTQConfig
+    gptq_config = GPTQConfig(bits=4, use_exllama=False)
+    model1 = AutoModelForCausalLM.from_pretrained("ISTA-DASLab/Llama-3-8B-Instruct-GPTQ-4bit", device_map="auto", quantization_config=gptq_config)
+    pass

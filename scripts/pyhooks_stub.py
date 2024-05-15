f4b15bc46c2da0cec1c1aad781a8a376fdc9309b
Milo Cress <milo.cress@databricks.com>
2024-05-15 01:19:52 +0000

    this is gross

--- scripts/pyhooks_stub.py
+++ scripts/pyhooks_stub.py
@@ -4,6 +4,7 @@
 # git clone git@github.com:databricks-mosaic/mcloud.git to /tmp/mcloud
 # git checkout pyhookbuffered
 import os
+import shutil
 import subprocess
 import sys
 import time
@@ -14,7 +15,8 @@ rank = os.environ.get('RANK')
 if rank is None:
     raise ValueError('RANK not set')
 
-try:
+if rank == '0':
+    shutil.rmtree('/tmp/mcloud', ignore_errors=True)
     subprocess.run([
         'git',
         'clone',
@@ -25,20 +27,14 @@ try:
     subprocess.run(['git', 'checkout', 'pyhookbuffered'],
                    cwd=f'/tmp/mcloud-{rank}',
                    check=True)
-except:
-    print('repo already exists, nbd')
-
-# add the following to the PYTHONPATH:
-# /tmp/mcloud/finetuning/pyhook
-sys.path.append(f'/tmp/mcloud-{rank}/finetuning/')
-
-# run `pip install fickling`
-try:
     subprocess.run(['pip', 'install', 'fickling'], check=True)
-except:
-    time.sleep(5)
 
-try:
-    import pyhook
-except:
-    raise ImportError('Failed to import pyhook from mcloud')
+    sys.path.append(f'/tmp/mcloud-{rank}/finetuning/')
+
+while True:
+    try:
+        import pyhook
+        break
+    except:
+        print('Failed to import pyhook from mcloud')
+        time.sleep(5)

c5d9f89454bea75cb2eb1a7cc38cf788fb2a1440
Chuck Tang <chuck.tang@databricks.com>
2024-05-15 09:35:19 -0700

    commit change

--- setup.py
+++ setup.py
@@ -59,7 +59,6 @@ install_requires = [
     'accelerate>=0.25,<0.26',  # for HF inference `device_map`
     'transformers>=4.40,<4.41',
     'mosaicml-streaming>=0.7.5,<0.8',
-    'torch>=2.3.0,<2.4',
     'datasets>=2.19,<2.20',
     'fsspec==2023.6.0',  # newer version results in a bug in datasets that duplicates data
     'sentencepiece==0.1.97',

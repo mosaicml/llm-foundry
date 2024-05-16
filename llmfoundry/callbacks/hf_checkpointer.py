a3bb94840072e5d7de6d957d020c34e79cc91a5d
Saaketh <narayan.saaketh@gmail.com>
2024-05-15 17:53:04 -0700

    no patch

--- llmfoundry/callbacks/hf_checkpointer.py
+++ llmfoundry/callbacks/hf_checkpointer.py
@@ -376,62 +376,62 @@ class HuggingFaceCheckpointer(Callback):
             state_dict_model = state.model.model
             original_tokenizer = state.model.tokenizer
 
-        if version.parse(torch.__version__) > version.parse('2.2.9'):
-            from torch.distributed._tensor import DTensor
-            from torch.distributed.checkpoint.state_dict import (
-                StateDictOptions,
-                get_model_state_dict,
-            )
-            cpu_offload = True
+        # if version.parse(torch.__version__) > version.parse('2.2.9'):
+        #     from torch.distributed._tensor import DTensor
+        #     from torch.distributed.checkpoint.state_dict import (
+        #         StateDictOptions,
+        #         get_model_state_dict,
+        #     )
+        #     cpu_offload = True
 
-            # Add a dtensor->cpu tensor hook to avoid CUDA OOM
-            def dtensor_to_tensor_hook(
-                module: nn.Module,
-                state_dict: Dict[str, Any],
-                prefix: str,
-                *args: Any,
-            ) -> Dict[str, Any]:
-                dtensor_fqns = []
-                for fqn in state_dict.keys():
-                    tensor = state_dict[fqn]
-                    if isinstance(tensor, DTensor):
-                        dtensor_fqns.append(fqn)
-                        tensor = tensor.full_tensor()  # type: ignore
-                        if dist.get_global_rank() == 0:
-                            if cpu_offload:
-                                tensor = tensor.cpu()
-                            state_dict[fqn] = tensor
-                if dist.get_global_rank() != 0:
-                    for fqn in dtensor_fqns:
-                        del state_dict[fqn]
-                return state_dict
+        #     # Add a dtensor->cpu tensor hook to avoid CUDA OOM
+        #     def dtensor_to_tensor_hook(
+        #         module: nn.Module,
+        #         state_dict: Dict[str, Any],
+        #         prefix: str,
+        #         *args: Any,
+        #     ) -> Dict[str, Any]:
+        #         dtensor_fqns = []
+        #         for fqn in state_dict.keys():
+        #             tensor = state_dict[fqn]
+        #             if isinstance(tensor, DTensor):
+        #                 dtensor_fqns.append(fqn)
+        #                 tensor = tensor.full_tensor()  # type: ignore
+        #                 if dist.get_global_rank() == 0:
+        #                     if cpu_offload:
+        #                         tensor = tensor.cpu()
+        #                     state_dict[fqn] = tensor
+        #         if dist.get_global_rank() != 0:
+        #             for fqn in dtensor_fqns:
+        #                 del state_dict[fqn]
+        #         return state_dict
 
-            hooks = []
-            for _, module in state_dict_model.named_modules():
-                if isinstance(module, FSDP):
-                    hooks.append(
-                        module.
-                        _register_state_dict_hook(dtensor_to_tensor_hook),
-                    )
+        #     hooks = []
+        #     for _, module in state_dict_model.named_modules():
+        #         if isinstance(module, FSDP):
+        #             hooks.append(
+        #                 module.
+        #                 _register_state_dict_hook(dtensor_to_tensor_hook),
+        #             )
 
-            state_dict = get_model_state_dict(
-                state_dict_model,
-                options=StateDictOptions(
-                    full_state_dict=True,
-                    cpu_offload=cpu_offload,
-                ),
-            )
-            for hook in hooks:
-                hook.remove()
-        else:
-            state_dict_context = fsdp_state_dict_type_context(
-                original_model,
-                state_dict_type='full',
-            ) if ((not state.is_model_ddp) and
-                  isinstance(state_dict_model,
-                             FSDP)) else contextlib.nullcontext()
-            with state_dict_context:
-                state_dict = state_dict_model.state_dict()
+        #     state_dict = get_model_state_dict(
+        #         state_dict_model,
+        #         options=StateDictOptions(
+        #             full_state_dict=True,
+        #             cpu_offload=cpu_offload,
+        #         ),
+        #     )
+        #     for hook in hooks:
+        #         hook.remove()
+        # else:
+        state_dict_context = fsdp_state_dict_type_context(
+            original_model,
+            state_dict_type='full',
+        ) if ((not state.is_model_ddp) and
+                isinstance(state_dict_model,
+                            FSDP)) else contextlib.nullcontext()
+        with state_dict_context:
+            state_dict = state_dict_model.state_dict()
 
         # Convert the state dict to the requested precis
         for k, v in state_dict.items():

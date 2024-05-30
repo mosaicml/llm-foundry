39afd5f491cf8c2e1bf136c1a7de2f2948fb6182
Ubuntu <shitao.li@your.hostname.com>
2024-05-30 22:16:02 +0000

    format

--- llmfoundry/data/finetuning/dataloader.py
+++ llmfoundry/data/finetuning/dataloader.py
@@ -542,7 +542,8 @@ def _download_remote_hf_dataset(remote_path: str, split: str) -> str:
             except FileNotFoundError as e:
                 if extension == SUPPORTED_EXTENSIONS[-1]:
                     files_searched = [
-                        f'{name[:-len(extension)]}{ext}' for ext in SUPPORTED_EXTENSIONS
+                        f'{name[:-len(extension)]}{ext}'
+                        for ext in SUPPORTED_EXTENSIONS
                     ]
                     raise FileNotFoundError(
                         f'Could not find a file with any of ' + \
@@ -740,12 +741,12 @@ if __name__ == '__main__':
                         print(
                             '\033[91m{}\033[00m\n'.format('TARGET:   '),
                             tokenizer.decode(
-                                batch['input_ids'][
-                                    j,
-                                    torch.logical_and(
-                                        is_subseq,
-                                        batch['labels'][j] != _HF_IGNORE_INDEX,
-                                    )],
+                                batch['input_ids'][j,
+                                                   torch.logical_and(
+                                                       is_subseq,
+                                                       batch['labels'][j] !=
+                                                       _HF_IGNORE_INDEX,
+                                                   )],
                                 skip_special_tokens=False,
                                 clean_up_tokenization_spaces=True,
                             ),
@@ -775,8 +776,8 @@ if __name__ == '__main__':
                     print(
                         '\033[91m{}\033[00m\n'.format('TARGET:   '),
                         tokenizer.decode(
-                            batch['input_ids'][
-                                j, batch['labels'][j] != _HF_IGNORE_INDEX],
+                            batch['input_ids'][j, batch['labels'][j] !=
+                                               _HF_IGNORE_INDEX],
                             skip_special_tokens=False,
                             clean_up_tokenization_spaces=True,
                         ),

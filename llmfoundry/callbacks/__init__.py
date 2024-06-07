d8fb44cbcbc1f40ce8eaae6222f8287589cbb05a
JackZ-db <jack.zhang@databricks.com>
2024-06-07 14:25:08 -0700

    fixed end of line error in __init__.py

--- llmfoundry/callbacks/__init__.py
+++ llmfoundry/callbacks/__init__.py
@@ -1,21 +1,3 @@
-# Copyright 2024 MosaicML LLM Foundry authors
-# SPDX-License-Identifier: Apache-2.0
-
-from composer.callbacks import SystemMetricsMonitor
-
-from llmfoundry.utils.builders import build_callback
-
-
-def test_system_metrics_monitor_callback_builds():
-    callback = build_callback(
-        'system_metrics_monitor',
-        kwargs={},
-        train_config={'train_loader': {}},
-    )
-    assert isinstance(callback, SystemMetricsMonitor)
-root@48eca11b-847e-4283-9557-ac7141834b29-0:/llm-foundry# cat llm-foundry/callbacks/__init__.py
-cat: llm-foundry/callbacks/__init__.py: No such file or directory
-root@48eca11b-847e-4283-9557-ac7141834b29-0:/llm-foundry# cat llmfoundry/callbacks/__init__.py
 # Copyright 2022 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 

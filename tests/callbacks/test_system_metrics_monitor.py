b9df1ed47fc58045b9d1396652f06b9aa220761e
JackZ-db <jack.zhang@databricks.com>
2024-06-07 14:03:43 -0700

    temporarily removing flag check in unit test for system_metrics_monitor build

--- tests/callbacks/test_system_metrics_monitor.py
+++ tests/callbacks/test_system_metrics_monitor.py
@@ -6,11 +6,9 @@ from llmfoundry.utils.builders import build_callback
 
 
 def test_system_metrics_monitor_callback_builds():
-    kwargs = {'log_all_data': True}
     callback = build_callback(
         'system_metrics_monitor',
-        kwargs=kwargs,
+        kwargs={},
         train_config={'train_loader': {}},
     )
     assert isinstance(callback, SystemMetricsMonitor)
-    assert getattr(callback, 'log_all_data', None) is True

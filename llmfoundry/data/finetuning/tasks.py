f46f6ffdb222b374c73ea8ad931968e6ebfc70da
Daniel King <daniel@mosaicml.com>
2024-06-13 15:18:36 -0700

    clean up

--- llmfoundry/data/finetuning/tasks.py
+++ llmfoundry/data/finetuning/tasks.py
@@ -649,7 +649,6 @@ class StreamingFinetuningDataset(StreamingDataset):
 
     # How to process a sample
     def __getitem__(self, idx: int) -> Dict[str, Any]:
-        print('Idx:', idx)
         sample = super().__getitem__(idx)
         if 'turns' in sample:
             # Already tokenized in latest format

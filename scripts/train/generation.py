f5bb13505ce652e9ac31640777f6d6d54d3e1204
Saaketh Narayan <narayan.saaketh@gmail.com>
2024-06-29 08:05:42 +0000

    tp

--- scripts/train/generation.py
+++ scripts/train/generation.py
@@ -502,26 +502,12 @@ def main(cfg: DictConfig) -> Trainer:
     def retrieve_layer_plan():
         layer_plan = {}
         for name, _ in model.named_modules():
-            # if 'Wqkv' in name or 'out_proj' in name:
-            #     print(f"using ColwiseParallel, [Replicate, Replicate] for module {name}")
-            #     layer_plan[name] = ColwiseParallel(
-            #         input_layouts = Replicate(),
-            #         output_layouts = Replicate(),
-            #     )
             if 'up_proj' in name or 'gate_proj' in name or 'Wqkv' in name or 'out_proj' in name or 'down_proj' in name:
                 print(f"using ColwiseParallel, [Replicate, Replicate] for module {name}")
                 layer_plan[name] = ColwiseParallel(
                     input_layouts = Replicate(),
                     output_layouts = Replicate(),
-                    #output_layouts = Shard(1),
                 )
-            # if 'down_proj' in name:
-            #     print(f"using RowwiseParallel, [Replicate, Replicate] for module {name}")
-            #     layer_plan[name] = RowwiseParallel(
-            #         #input_layouts = Shard(1),
-            #         input_layouts = Replicate(),
-            #         output_layouts = Replicate(),
-            #     )
         return layer_plan
     
     # ADDED TP CONFIG:

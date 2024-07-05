de6f2c883fe24d304b49ff29b2d5473090d842d1
v-chen_data <v-chen_data@example.com>
2024-07-05 14:52:29 -0700

    test

--- llmfoundry/cli/cli.py
+++ llmfoundry/cli/cli.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import typer
+import sys
 
 from llmfoundry.cli import registry_cli
 from llmfoundry.train.train import run_trainer
@@ -10,11 +11,9 @@ app = typer.Typer(pretty_exceptions_show_locals=False)
 app.add_typer(registry_cli.app, name='registry')
 
 @app.command(name="train")
-def train(
-    yaml_path: str = typer.Argument(..., help="Path to the YAML configuration file"),
-    args_list: list = typer.Option(None, help="Additional command line arguments", hidden=True)
-):
+def train(yaml_path: str = typer.Argument(..., help="Path to the YAML configuration file")):
     """Run the training with the given configuration and optional overrides from command line."""
+    args_list = sys.argv[2:]  # assuming additional arguments may follow the yaml_path
     run_trainer(yaml_path, args_list)
 
 if __name__ == '__main__':

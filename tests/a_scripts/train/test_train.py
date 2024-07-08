fada7ffd95cc5bbc7ac7eff5d2df40a228e87605
v-chen_data <v-chen_data@example.com>
2024-07-08 11:11:01 -0700

    typo

--- tests/a_scripts/train/test_train.py
+++ tests/a_scripts/train/test_train.py
@@ -15,8 +15,8 @@ from llmfoundry.utils.config_utils import (
     make_dataclass_and_log_config,
     update_batch_size_info,
 )
-from scripts.train.train import TrainConfig  # noqa: E402
-from scripts.train.train import TRAIN_CONFIG_KEYS, main, validate_config
+from llmfoundry.train.train import TrainConfig  # noqa: E402
+from llmfoundry.train.train import TRAIN_CONFIG_KEYS, main, validate_config
 from tests.data_utils import create_c4_dataset_xxsmall, gpt_tiny_cfg
 from tests.fixtures.autouse import REPO_DIR
 

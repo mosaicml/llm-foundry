d1d84794bff43bfd1257b34ca01aa446cdffe2e9
v-chen_data <v-chen_data@example.com>
2024-07-08 12:44:24 -0700

    precommit

--- llmfoundry/cli/cli.py
+++ llmfoundry/cli/cli.py
@@ -1,7 +1,7 @@
 # Copyright 2024 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
-from typing import List
+from typing import List, Optional
 
 import typer
 from omegaconf import DictConfig
@@ -17,11 +17,11 @@ app.add_typer(registry_cli.app, name='registry')
 @app.command(name='train')
 def train(
     yaml_path: str = typer.Argument(
-        ..., help='Path to the YAML configuration file'
-    ),
-    args_list: Optional[
-        List[str]
-    ] = typer.Argument(None, help='Additional command line arguments'),
+        ...,
+        help='Path to the YAML configuration file',
+    ),  # type: ignore
+    args_list: Optional[List[str]] = typer.
+    Argument(None, help='Additional command line arguments'),  # type: ignore
 ):
     """Run the training with optional overrides from CLI."""
     om.clear_resolver('oc.env')

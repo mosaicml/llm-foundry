e13e50e066c88cb079f1b90d8a442e203edea44b
jjanezhang <jane.zhang@databricks.com>
2024-07-08 17:00:52 -0700

    test timeout without try catch

--- llmfoundry/utils/__init__.py
+++ llmfoundry/utils/__init__.py
@@ -46,7 +46,6 @@ from llmfoundry.utils.mosaicml_logger_utils import (
     log_eval_analytics,
     log_train_analytics,
     maybe_create_mosaicml_logger,
-    no_override_excepthook,
 )
 from llmfoundry.utils.prompt_files import load_prompts, load_prompts_from_file
 from llmfoundry.utils.registry_utils import (
@@ -98,7 +97,6 @@ __all__ = [
     'download_from_hf_hub',
     'download_from_oras',
     'maybe_create_mosaicml_logger',
-    'no_override_excepthook',
     'find_mosaicml_logger',
     'log_eval_analytics',
     'log_train_analytics',

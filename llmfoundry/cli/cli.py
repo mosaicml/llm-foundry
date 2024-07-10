7e5bf89429b1f56f1de6c81663ecca64743964a4
v-chen_data <v-chen_data@example.com>
2024-07-10 12:03:05 -0700

    rm type ignore

--- llmfoundry/cli/cli.py
+++ llmfoundry/cli/cli.py
@@ -17,9 +17,10 @@ def train(
     yaml_path: str = typer.Argument(
         ...,
         help='Path to the YAML configuration file',
-    ),  # type: ignore
-    args_list: Optional[List[str]] = typer.
-    Argument(None, help='Additional command line arguments'),  # type: ignore
+    ),
+    args_list: Optional[
+        List[str]
+    ] = typer.Argument(None, help='Additional command line arguments'),
 ):
     """Run the training with optional overrides from CLI."""
     train_from_yaml(yaml_path, args_list)

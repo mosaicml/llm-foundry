2d8d75c72bf7b49919b6ea544f31fd852bd1a86d
v-chen_data <v-chen_data@example.com>
2024-07-09 18:33:24 -0700

    import pathing

--- llmfoundry/train/__init__.py
+++ llmfoundry/train/__init__.py
@@ -1,3 +1,9 @@
 # Copyright 2024 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
-from llmfoundry.train.train import train, train_from_yaml
+from llmfoundry.train.train import (
+    train, 
+    train_from_yaml,
+    TrainConfig,
+    TRAIN_CONFIG_KEYS,
+    validate_config
+)

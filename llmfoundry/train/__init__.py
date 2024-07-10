a42506220c96b9ff48a9d6bc54dfc2437928c294
v-chen_data <v-chen_data@example.com>
2024-07-10 11:55:06 -0700

    precommit

--- llmfoundry/train/__init__.py
+++ llmfoundry/train/__init__.py
@@ -1,11 +1,11 @@
 # Copyright 2024 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 from llmfoundry.train.train import (
-    train, 
-    train_from_yaml,
-    TrainConfig,
     TRAIN_CONFIG_KEYS,
-    validate_config
+    TrainConfig,
+    train,
+    train_from_yaml,
+    validate_config,
 )
 
 __all__ = [
@@ -13,5 +13,5 @@ __all__ = [
     'train_from_yaml',
     'TrainConfig',
     'TRAIN_CONFIG_KEYS',
-    'validate_config'
+    'validate_config',
 ]

f46e5842acca7f7fc5d85060df854ec1a31740ef
v-chen_data <v-chen_data@example.com>
2024-07-09 18:08:55 -0700

    resolve 2/3 comments

--- llmfoundry/train/__init__.py
+++ llmfoundry/train/__init__.py
@@ -1,2 +1,3 @@
 # Copyright 2024 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
+from llmfoundry.train.train import train
--- llmfoundry/train/train.py
+++ llmfoundry/train/train.py
@@ -563,18 +563,3 @@ def train(cfg: DictConfig) -> Trainer:
 
     log.info('Done.')
     return trainer
-
-
-if __name__ == '__main__':
-    yaml_path, args_list = sys.argv[1], sys.argv[2:]
-
-    # Disable resolving environment variables through omegaconf.
-    om.clear_resolver('oc.env')
-
-    # Load yaml and cli arguments.
-    with open(yaml_path) as f:
-        yaml_cfg = om.load(f)
-    cli_cfg = om.from_cli(args_list)
-    cfg = om.merge(yaml_cfg, cli_cfg)
-    assert isinstance(cfg, DictConfig)
-    train(cfg)

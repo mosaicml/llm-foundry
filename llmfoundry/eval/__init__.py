5b080057e6d2568f041af0bd6b074306d6a7a7f6
v-chen_data <v-chen_data@example.com>
2024-07-11 11:28:30 -0700

    revert

--- llmfoundry/eval/__init__.py
+++ llmfoundry/eval/__init__.py
@@ -1,11 +1,33 @@
 # Copyright 2024 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
-from llmfoundry.eval.eval import (
-    evaluate,
-    eval_from_yaml,
+from llmfoundry.eval.datasets.in_context_learning_evaluation import (
+    InContextLearningDataset,
+    InContextLearningGenerationTaskWithAnswersDataset,
+    InContextLearningLMTaskDataset,
+    InContextLearningMultipleChoiceTaskDataset,
+    InContextLearningSchemaTaskDataset,
+    get_icl_task_dataloader,
+)
+from llmfoundry.eval.metrics.nlp import (
+    InContextLearningGenerationExactMatchAccuracy,
+    InContextLearningLMAccuracy,
+    InContextLearningLMExpectedCalibrationError,
+    InContextLearningMCExpectedCalibrationError,
+    InContextLearningMetric,
+    InContextLearningMultipleChoiceAccuracy,
 )
 __all__ = [
-    'evaluate',
-    'eval_from_yaml',
+    'InContextLearningDataset',
+    'InContextLearningLMTaskDataset',
+    'InContextLearningMultipleChoiceTaskDataset',
+    'InContextLearningSchemaTaskDataset',
+    'InContextLearningGenerationTaskWithAnswersDataset',
+    'get_icl_task_dataloader',
+    'InContextLearningMetric',
+    'InContextLearningLMAccuracy',
+    'InContextLearningMultipleChoiceAccuracy',
+    'InContextLearningGenerationExactMatchAccuracy',
+    'InContextLearningLMExpectedCalibrationError',
+    'InContextLearningMCExpectedCalibrationError',
 ]

50993da2dcaad11320acda5faa94ee31f37ff29d
Kushal Kodnad <kushal.kodnad@databricks.com>
2024-07-12 17:22:52 +0000

    Add ChronosTokenizerWrapper as a package

--- llmfoundry/tokenizers/__init__.py
+++ llmfoundry/tokenizers/__init__.py
@@ -2,7 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from llmfoundry.tokenizers.tiktoken import TiktokenTokenizerWrapper
+from llmfoundry.tokenizers.chronos_tokenizer import ChronosTokenizerWrapper
 
 __all__ = [
     'TiktokenTokenizerWrapper',
+    'ChronosTokenizerWrapper',
 ]

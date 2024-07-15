5da54c697c64bcc822acf593c73182a273bd4e09
Kushal Kodnad <kushal.kodnad@databricks.com>
2024-07-15 17:38:51 +0000

    Added logging to train script

--- scripts/train/train.py
+++ scripts/train/train.py
@@ -1,9 +1,11 @@
 # Copyright 2022 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 import sys
+import logging
 
 from llmfoundry.train import train_from_yaml
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='./tmp/train_logfile.log', filemode='w', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
     yaml_path, args_list = sys.argv[1], sys.argv[2:]
     train_from_yaml(yaml_path, args_list)

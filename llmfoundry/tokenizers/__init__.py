c08168e595514b4c8abc1f982a6223e334aed033
Kushal Kodnad <170473237+kushalkodn-db@users.noreply.github.com>
2024-07-22 17:50:44 -0700

    Register tiktoken to tokenizers registry

--- llmfoundry/tokenizers/__init__.py
+++ llmfoundry/tokenizers/__init__.py
@@ -1,7 +1,10 @@
 # Copyright 2022 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
+from llmfoundry.registry import tokenizers
 from llmfoundry.tokenizers.tiktoken import TiktokenTokenizerWrapper
+
+tokenizers.register('tiktoken', func=TiktokenTokenizerWrapper)
 
 __all__ = [
     'TiktokenTokenizerWrapper',

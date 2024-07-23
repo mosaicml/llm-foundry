6800bb032accb62c869c3d4a3a887f80159c9794
Kushal Kodnad <170473237+kushalkodn-db@users.noreply.github.com>
2024-07-22 17:56:57 -0700

    Update build_tokenizer() to use the tokenizers registry

--- llmfoundry/utils/builders.py
+++ llmfoundry/utils/builders.py
@@ -506,57 +506,6 @@ def build_tokenizer(
         with dist.local_rank_zero_download_and_wait(signal_file_path):
             pass
 
-    if tokenizer_name.startswith('tiktoken'):
-        tokenizer = TiktokenTokenizerWrapper(**tokenizer_kwargs)
-    else:
-        tokenizer = AutoTokenizer.from_pretrained(
-            tokenizer_name,
-            **tokenizer_kwargs,
-        )
-
-        # HuggingFace does not respect the model_max_length kwarg, and overrides it with
-        # min(kwargs['model_max_length'], original_config['model_max_length']), so we
-        # explicitly set it here
-        tokenizer.model_max_length = tokenizer_kwargs.get(
-            'model_max_length',
-            int(1e30),
-        )
-
-    if not hasattr(tokenizer, 'eos_token') or tokenizer.eos_token is None:
-        raise ValueError(
-            f'The tokenizer {tokenizer_name} must have an eos_token.',
-        )
-
-    if dist.is_available() and dist.is_initialized(
-    ) and dist.get_world_size() > 1:
-        if dist.get_local_rank() == 0:
-            with open(signal_file_path, 'wb') as f:
-                f.write(b'local_rank0_completed_tokenizer_setup')
-
-        dist.barrier()
-
-        if dist.get_local_rank() == 0:
-            os.remove(signal_file_path)
-
-    return tokenizer
-
-
-def build_tokenizer_from_registry(  # TODO: Rename this to ``build_tokenizer`` once approach is approved
-    tokenizer_name: str,
-    tokenizer_kwargs: Dict[str, Any],
-) -> PreTrainedTokenizerBase:
-    """Builds a tokenizer from the registry."""
-    os.environ['TRANSFORMERS_NO_ADVISORY_WARNINGS'] = '1'
-    os.environ['TOKENIZERS_PARALLELISM'] = 'false'
-
-    signal_file_path = f'.node_{dist.get_node_rank()}_local_rank0_completed_tokenizer_setup'
-
-    if dist.is_available() and dist.is_initialized(
-    ) and dist.get_world_size() > 1:
-        # Make sure the tokenizer files are downloaded and cached first by local rank 0
-        with dist.local_rank_zero_download_and_wait(signal_file_path):
-            pass
-
     if tokenizer_name in registry.tokenizers:
         tokenizer = construct_from_registry(
             name=tokenizer_name,

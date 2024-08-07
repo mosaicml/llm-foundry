319a7f167e592dae195a05d839c1f8e94e3e7b2f
Daniel King <daniel@mosaicml.com>
2024-08-06 17:36:17 -0700

    special case split

--- llmfoundry/data/finetuning/dataloader.py
+++ llmfoundry/data/finetuning/dataloader.py
@@ -301,7 +301,7 @@ def build_finetuning_dataloader(
             **{
                 k: v
                 for k, v in dataset_cfg.items()
-                if k in dataset_constructor_keys
+                if k in dataset_constructor_keys and k != 'split'
             },
         )
 

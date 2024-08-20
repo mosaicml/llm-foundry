7b4d31e2741cd2f01a394a976459e0dde29a8253
Joyce Chen <joyce.chen@databricks.com>
2024-08-19 17:15:05 -0700

    add a condition for stopping if generally very high losses

--- llmfoundry/callbacks/kill_loss_spike_callback.py
+++ llmfoundry/callbacks/kill_loss_spike_callback.py
@@ -16,10 +16,11 @@ __all__ = ['KillLossSpike']
 
 class KillLossSpike(Callback):
 	
-    def __init__(self, patience:int=3, outlier_multiplier:int=2, window_size:int=100):
+    def __init__(self, patience:int=3, outlier_multiplier:int=2, window_size:int=100, loss_cap:int=10):
         self.patience = patience
         self.outlier_multiplier = outlier_multiplier
         self.window_size = window_size
+        self.loss_cap = loss_cap
         self.outlier_counter = 0
         self.loss_window = []
 
@@ -43,13 +44,18 @@ class KillLossSpike(Callback):
                 self.outlier_counter += 1
                 log.info(f'Potential loss spike detected. Iteration: {self.outlier_counter}')
                 if self.outlier_counter > self.patience:
-                    # Some kind of user error message
-                    raise LossSpikeError(self.outlier_multiplier, round(running_loss_avg), self.outlier_counter)
+                    # raise LossSpikeError(self.outlier_multiplier, round(running_loss_avg), self.outlier_counter)
+                    log.info(f'RUN STOPPED. Loss spike >{self.outlier_multiplier}*{running_loss_avg} detected for {self.outlier_counter} steps.')
 
             # Previous step loss was an outlier, current step loss is not. Reset outlier counter.
             elif self.outlier_counter > 0:
                 log.info(f'Not a persistent loss spike. Resetting outlier counter.')
                 self.outlier_counter = 0
+
+            # Half of the running losses are greater than our "high loss" threshold
+            elif sum(1 for loss in self.loss_window if loss > self.loss_cap) >= self.window_size / 2:
+                # raise LossSpikeError()
+                log.info(f'RUN STOPPED. High losses >{self.loss_cap} detected.')
 
         else:
             log.info(f'Full loss window size not reached ({len(self.loss_window)} < {self.window_size}). Collecting loss data...')

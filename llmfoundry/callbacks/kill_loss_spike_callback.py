f8fe2d8ac44c0d1fb001eec93fe88f87272c71b3
Joyce Chen <joyce.chen@databricks.com>
2024-08-22 12:59:26 -0700

    lint

--- llmfoundry/callbacks/kill_loss_spike_callback.py
+++ llmfoundry/callbacks/kill_loss_spike_callback.py
@@ -4,20 +4,23 @@
 """Track training runs for loss spikes or persistently high training loss."""
 from __future__ import annotations
 
-import torch
 import logging
-import numpy as np
 from collections import deque
+
+import numpy as np
+import torch
 from composer.core import Callback, State
 from composer.loggers import Logger, MosaicMLLogger
+
 from llmfoundry.utils.exceptions import LossSpikeError
+
 log = logging.getLogger(__name__)
 
 __all__ = ['KillLossSpike']
 
+
 class KillLossSpike(Callback):
-    """
-    This callback detects and handles loss spikes or persistently high training losses during model training.
+    """Detects and handles loss spikes or persistently high training losses during model training.
 
     Monitors the training loss at the end of each batch and maintains a rolling window of recent losses.
     If recent training losses exceed a specified cap or if a significant spike in loss is detected, the callback can either
@@ -33,15 +36,22 @@ class KillLossSpike(Callback):
                                   outlier if it is outlier_multiplier times greater than the mean of losses in
                                   the current window. Default is 2.
         window_size (int): The size of the rolling window used to track recent losses. Default is 100.
-        loss_cap (int): The maximum allowable loss. If the training loss consistently exceeds this value, 
+        loss_cap (int): The maximum allowable loss. If the training loss consistently exceeds this value,
                         it is considered a diverging or unstable run. Default is 10.
 
     Raises:
         LossSpikeError: If log_only is False and a loss spike or persistently high loss is detected, this error is
                         raised to stop the run with an error message.
     """
-	
-    def __init__(self, log_only:bool, patience:int=4, outlier_multiplier:int=2, window_size:int=100, loss_cap:int=10):
+
+    def __init__(
+        self,
+        log_only: bool,
+        patience: int = 4,
+        outlier_multiplier: int = 2,
+        window_size: int = 100,
+        loss_cap: int = 10,
+    ):
         self.log_only = log_only
         self.patience = patience
         self.outlier_multiplier = outlier_multiplier
@@ -54,20 +64,29 @@ class KillLossSpike(Callback):
         # Train loss is an outlier
         if train_loss >= running_loss_avg * self.outlier_multiplier:
             self.outlier_counter += 1
-            log.info(f'Potential loss spike detected. Iteration: {self.outlier_counter}')
+            log.info(
+                f'Potential loss spike detected. Iteration: {self.outlier_counter}',
+            )
             if self.outlier_counter > self.patience:
-                log.info(f'Loss spike detected for {self.outlier_counter} steps. Try lowering the learning rate.')
+                log.info(
+                    f'Loss spike detected for {self.outlier_counter} steps. Try lowering the learning rate.',
+                )
                 return True
         # Previous step loss was an outlier, current step loss is not. Reset outlier counter.
         elif self.outlier_counter > 0:
             log.info(f'Not a persistent loss spike. Resetting outlier counter.')
             self.outlier_counter = 0
         return False
-    
+
     def detect_high_losses(self, current_step: int):
         # Half of the running losses are greater than our "high loss" threshold, after an initial buffer period
-        if (current_step >= self.window_size * 2) and (sum(1 for loss in self.loss_window if loss > self.loss_cap) >= self.window_size / 2):
-            log.info(f'High losses (train loss consistently greater than {self.loss_cap}) detected.')
+        if (current_step >= self.window_size * 2) and (
+            sum(1 for loss in self.loss_window if loss > self.loss_cap) >=
+            self.window_size / 2
+        ):
+            log.info(
+                f'High losses (train loss consistently greater than {self.loss_cap}) detected.',
+            )
             return True
         return False
 
@@ -86,16 +105,29 @@ class KillLossSpike(Callback):
                 if self.log_only:
                     for destination in logger.destinations:
                         if isinstance(destination, MosaicMLLogger):
-                            destination.log_metadata({'loss_spike': f'Training loss spike detected for {self.outlier_counter} consecutive steps.'})
+                            destination.log_metadata({
+                                'loss_spike':
+                                    f'Training loss spike detected for {self.outlier_counter} consecutive steps.',
+                            })
                 else:
-                    raise LossSpikeError(outlier_multiplier=self.outlier_multiplier, running_loss_avg=round(running_loss_avg), outlier_counter=self.outlier_counter)
+                    raise LossSpikeError(
+                        outlier_multiplier=self.outlier_multiplier,
+                        running_loss_avg=round(running_loss_avg),
+                        outlier_counter=self.outlier_counter,
+                    )
 
             elif self.detect_high_losses(int(state.timestamp.batch)):
                 if self.log_only:
                     for destination in logger.destinations:
                         if isinstance(destination, MosaicMLLogger):
-                            destination.log_metadata({'high_loss': f'Persistently high (>{self.loss_cap}) training losses detected.'})
+                            destination.log_metadata({
+                                'high_loss':
+                                    f'Persistently high (>{self.loss_cap}) training losses detected.',
+                            })
                 else:
-                    raise LossSpikeError(loss_cap=self.loss_cap, window_size=self.window_size)
+                    raise LossSpikeError(
+                        loss_cap=self.loss_cap,
+                        window_size=self.window_size,
+                    )
 
         self.loss_window.append(train_loss)
--- tests/callbacks/test_kill_loss_spike_callback.py
+++ tests/callbacks/test_kill_loss_spike_callback.py
@@ -1,14 +1,23 @@
 # Copyright 2024 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 import unittest
-from unittest.mock import patch
-from llmfoundry.callbacks.kill_loss_spike_callback import KillLossSpike
 from collections import deque
+from unittest.mock import patch
+
+from llmfoundry.callbacks.kill_loss_spike_callback import KillLossSpike
+
 
 class TestKillLossSpike(unittest.TestCase):
-    def __init__(self, *args:str, **kwargs:dict):
+
+    def __init__(self, *args: str, **kwargs: dict):
         super(TestKillLossSpike, self).__init__(*args, **kwargs)
-        self.callback = KillLossSpike(log_only=True, patience=4, outlier_multiplier=2, window_size=10, loss_cap=10)
+        self.callback = KillLossSpike(
+            log_only=True,
+            patience=4,
+            outlier_multiplier=2,
+            window_size=10,
+            loss_cap=10,
+        )
 
     @patch('llmfoundry.callbacks.kill_loss_spike_callback.log')
     def test_detect_loss_spike_no_spike(self, _):
@@ -35,10 +44,14 @@ class TestKillLossSpike(unittest.TestCase):
 
     @patch('llmfoundry.callbacks.kill_loss_spike_callback.log')
     def test_detect_high_losses_with_high_losses(self, _):
-        self.callback.loss_window = deque([9, 8, 7, 6, 5, 11, 12, 13, 14, 15], maxlen=10)  # Simulate mix of losses in loss window
+        self.callback.loss_window = deque(
+            [9, 8, 7, 6, 5, 11, 12, 13, 14, 15],
+            maxlen=10,
+        )  # Simulate mix of losses in loss window
         current_step = 21
         result = self.callback.detect_high_losses(current_step)
         self.assertTrue(result)
+
 
 if __name__ == '__main__':
     unittest.main()

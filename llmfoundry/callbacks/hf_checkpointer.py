594bb20709cb56ffb356fb3a8c2fe45985494262
Milo Cress <milo.cress@databricks.com>
2024-08-27 16:32:43 +0000

    fix hf checkpointer?

--- llmfoundry/callbacks/hf_checkpointer.py
+++ llmfoundry/callbacks/hf_checkpointer.py
@@ -652,6 +652,11 @@ class HuggingFaceCheckpointer(Callback):
 
                     self.pre_register_edit(local_save_path,)
 
+                    # Save the monitor process to be restored after registering the model.
+                    if hasattr(mlflow_logger, 'monitor_process'):
+                        monitor_process = mlflow_logger.monitor_process
+                        mlflow_logger.monitor_process = None
+
                     # Spawn a new process to register the model.
                     process = SpawnProcess(
                         target=_register_model_with_run_id_multiprocess,
@@ -669,6 +674,9 @@ class HuggingFaceCheckpointer(Callback):
                         },
                     )
                     process.start()
+
+                    # Restore the monitor process.
+                    mlflow_logger.monitor_process = monitor_process
                     self.child_processes.append(process)
 
                     # Save the temporary directory to be cleaned up later.

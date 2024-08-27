e771fac9a311f438b69b7aba53c0976d5d815036
Saaketh <narayan.saaketh@gmail.com>
2024-08-27 00:07:46 -0400

    yo

--- tests/utils/test_config_utils.py
+++ tests/utils/test_config_utils.py
@@ -1,10 +1,13 @@
 # Copyright 2024 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
-from llmfoundry.utils.config_utils import update_config_with_batch_size_info, process_init_device
-from composer.utils import dist
 from unittest.mock import patch
+
 import pytest
+from composer.utils import dist
+
+from llmfoundry.utils.config_utils import (process_init_device,
+                                           update_config_with_batch_size_info,)
 
 
 def test_update_config_with_batch_size_info():
@@ -17,13 +20,14 @@ def test_update_config_with_batch_size_info():
     assert config['device_train_grad_accum'] == 3
     assert config['device_eval_batch_size'] == 2
 
+
 @pytest.mark.parametrize('shard_degree_specified', [True, False])
 @pytest.mark.parametrize('replicate_degree_specified', [True, False])
 @pytest.mark.parametrize('should_shard_only', [True, False])
 def test_moe_fsdp_config_ffn_config(
     shard_degree_specified: bool,
     replicate_degree_specified: bool,
-    should_shard_only: bool
+    should_shard_only: bool,
 ):
     model_cfg = {
         'moe_world_size': 4,
@@ -31,7 +35,7 @@ def test_moe_fsdp_config_ffn_config(
         'fc_type': 'torch',
         'ffn_config': {
             'ffn_type': 'mb_moe',
-        }
+        },
     }
     fsdp_config = {}
     if shard_degree_specified and replicate_degree_specified:
@@ -51,12 +55,14 @@ def test_moe_fsdp_config_ffn_config(
             fsdp_config['data_parallel_replicate_degree'] = 1
         else:
             fsdp_config['data_parallel_replicate_degree'] = 2
-    
+
     # Ensure the ffn_config's device_mesh is set correctly using the fsdp_config
     with patch('composer.utils.dist.get_world_size', return_value=8):
         _ = process_init_device(model_cfg, fsdp_config)
 
-        if should_shard_only or (not shard_degree_specified and not replicate_degree_specified):
+        if should_shard_only or (
+            not shard_degree_specified and not replicate_degree_specified
+        ):
             assert model_cfg['ffn_config']['device_mesh'] == [8]
         else:
             assert model_cfg['ffn_config']['device_mesh'] == [2, 4]

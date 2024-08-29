07bec5a741359ace1395e2e31f655394d82fa21e
joyce-chen-uni <joyce.chen@databricks.com>
2024-08-29 15:29:11 -0700

    Fix formatting of loss spike & high loss error messages (#1498)

--- llmfoundry/utils/exceptions.py
+++ llmfoundry/utils/exceptions.py
@@ -399,9 +399,7 @@ class LossSpikeError(UserError):
         outlier_counter: int,
         loss_window: list[float],
     ) -> None:
-        message = f'Training stopped due to a loss spike. The training loss was more than {outlier_multiplier} times greater than \
-                    the running average loss (approx. {round(running_loss_avg, 1)}) over {outlier_counter} consecutive training steps. \
-                    Please try submitting the run again with a lower learning rate.'
+        message = f'Training stopped due to a loss spike. The training loss was more than {outlier_multiplier} times greater than the running average loss (approx. {running_loss_avg}) over {outlier_counter} consecutive training steps. Please try submitting the run again with a lower learning rate.'
 
         super().__init__(
             message,
@@ -421,8 +419,7 @@ class HighLossError(UserError):
         window_size: int,
         loss_window: list[float],
     ) -> None:
-        message = f'Training stopped due to consistently high losses. The training loss exceeded the threshold of {loss_cap} \
-                        for more than half of the {window_size} most recent training steps. Please try submitting the run again with a lower learning rate.'
+        message = f'Training stopped due to consistently high losses. The training loss exceeded the threshold of {loss_cap} for more than half of the {window_size} most recent training steps. Please try submitting the run again with a lower learning rate.'
 
         super().__init__(
             message,

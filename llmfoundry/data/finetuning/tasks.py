e08082d27f23ed6fe63e59488bdc29f27b4a5ec8
xiaohanzhangcmu <xiaohanzhang.cmu@gmail.com>
2024-09-14 00:11:13 -0700

    update

--- llmfoundry/data/finetuning/tasks.py
+++ llmfoundry/data/finetuning/tasks.py
@@ -633,6 +633,8 @@ class StreamingFinetuningDataset(StreamingDataset):
                     split,
                 )
 
+        print(f'I am here 1 {kwargs=}')
+
         super().__init__(
             streams=streams,
             local=local,

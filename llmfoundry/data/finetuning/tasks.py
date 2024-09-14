6a3be7b1bb744a156cae25a5b1757b904a25d4dd
xiaohanzhangcmu <xiaohanzhang.cmu@gmail.com>
2024-09-14 00:06:34 -0700

    update

--- llmfoundry/data/finetuning/tasks.py
+++ llmfoundry/data/finetuning/tasks.py
@@ -657,6 +657,7 @@ class StreamingFinetuningDataset(StreamingDataset):
             batching_method=batching_method,
             allow_unsafe_types=allow_unsafe_types,
             replication=replication,
+            **kwargs,
         )
 
         self.tokenizer = tokenizer

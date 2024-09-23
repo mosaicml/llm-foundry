8537a6cc8bdf0ee7258f0c7a2d323ff1f596f998
Daniel King <daniel@mosaicml.com>
2024-09-23 15:10:45 -0700

    pc

--- tests/loggers/test_mosaic_ml_logger.py
+++ tests/loggers/test_mosaic_ml_logger.py
@@ -8,7 +8,8 @@ from llmfoundry.utils.builders import build_logger
 
 def test_mosaic_ml_logger_constructs():
     mosaic_ml_logger = build_logger(
-        'mosaicml', kwargs={'ignore_exceptions': True}
+        'mosaicml',
+        kwargs={'ignore_exceptions': True},
     )
 
     assert isinstance(mosaic_ml_logger, MosaicMLLogger)

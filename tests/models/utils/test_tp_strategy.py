5e58dbc62bdd04eff838a954cc6cca90fae9d370
Eitan Turok <eitan.turok@gmail.com>
2024-09-25 20:12:54 +0000

    add test for tp training

--- tests/models/utils/test_tp_strategy.py
+++ tests/models/utils/test_tp_strategy.py
@@ -1,5 +1,10 @@
+import pytest
+
+from composer.trainer.trainer import Trainer
+from composer.utils import dist
 from torch.distributed.tensor.parallel import ColwiseParallel, RowwiseParallel, PrepareModuleInput
 from torch.distributed._tensor import Replicate, Shard
+from torch.utils.data import DataLoader
 
 from llmfoundry.models.mpt.modeling_mpt import ComposerMPTCausalLM
 from llmfoundry.utils.builders import build_tp_strategy
@@ -8,8 +13,9 @@ from llmfoundry.utils.builders import build_tp_strategy
 from icecream import install
 install()
 
-def test_tp_strategy():
+def test_ffn_tp_strategy_layer_plan():
 
+    # Actual layer plan
     tp_config = {
         'strategy': 'ffn',
         }
@@ -26,7 +32,10 @@ def test_tp_strategy():
             'attn_impl': 'flash',
         },
     }
+    model = ComposerMPTCausalLM(**model_cfg)
+    layer_plan = build_tp_strategy(tp_config['strategy'], model)
 
+    # Expected layer plan
     _expected_layer_plan = {
         'ffn': PrepareModuleInput(
                 input_layouts = Shard(0),
@@ -44,10 +53,7 @@ def test_tp_strategy():
     }
     expected_layer_plan = {f'model.transformer.blocks.{layer_idx}.{name}': layer_plan for name, layer_plan in _expected_layer_plan.items() for layer_idx in range(model_cfg['n_layers'])}
 
-    model = ComposerMPTCausalLM(**model_cfg)
-    layer_plan = build_tp_strategy(tp_config['strategy'], model)
-
-    # Compare expected and actual layer plan
+    # Compare expected and actual layer plans
     for (n1, lp1), (n2, lp2) in zip(sorted(expected_layer_plan.items()), sorted(layer_plan.items())):
         assert n1 == n2
         assert type(lp1) == type(lp2)
@@ -60,5 +66,50 @@ def test_tp_strategy():
             assert lp1.output_layouts == lp2.output_layouts
             assert lp1.use_local_output == lp2.use_local_output
 
-if __name__ == '__main__':
-    test_tp_strategy()
+
+@pytest.mark.gpu
+@pytest.mark.world_size(4)
+def test_tp_train(tiny_ft_dataloader: DataLoader):
+    ic(tiny_ft_dataloader)
+
+    # Actual layer plan
+    tp_config = {
+        'strategy': 'ffn',
+        }
+
+    model_cfg = {
+        'name': 'mpt_causal_lm',
+        'd_model': 128,
+        'n_heads': 4,
+        'n_layers': 3,
+        'expansion_ratio': 1,
+        'max_seq_len': 16,
+        'vocab_size': 50368,
+        'attn_config': {
+            'attn_impl': 'flash',
+        },
+    }
+    model = ComposerMPTCausalLM(**model_cfg)
+    layer_plan = build_tp_strategy(tp_config['strategy'], model)
+
+    # dataset = RandomClassificationDataset(size=8)
+    # dataloader = DataLoader(dataset, batch_size=2, sampler=dist.get_sampler(dataset))
+
+    trainer = Trainer(
+        model=model,
+        train_dataloader=tiny_ft_dataloader,
+        parallelism_config={
+            'tp': {
+                'layer_plan': layer_plan,
+                'tensor_parallel_degree': 2,
+            },
+            'fsdp': {},
+        },
+        max_duration='3ba',
+    )
+
+    trainer.fit()
+
+
+# if __name__ == '__main__':
+#     test_tp_train()

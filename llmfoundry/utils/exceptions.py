8cf3d8718763e7d9760b6f4df5780e6c23e18e0f
Brian <23239305+b-chu@users.noreply.github.com>
2024-10-01 14:12:55 -0400

    Add FileExtensionNotFoundError (#1564)

--- llmfoundry/utils/exceptions.py
+++ llmfoundry/utils/exceptions.py
@@ -481,3 +481,19 @@ class FaultyDataPrepCluster(UserError):
 
     def __str__(self):
         return self.message
+
+
+class FinetuningFileNotFoundError(UserError):
+    """Error thrown when a file can't be found with any supported extension."""
+
+    def __init__(self, files_searched: list[str]) -> None:
+        from llmfoundry.data.finetuning.tasks import SUPPORTED_EXTENSIONS
+        message = (
+            f'Could not find a file with any of ' + \
+            f'the supported extensions: {SUPPORTED_EXTENSIONS}\n' + \
+            f'at {files_searched}'
+        )
+        super().__init__(
+            message,
+            files_searched=files_searched,
+        )

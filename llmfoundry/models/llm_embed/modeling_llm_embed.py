7600b169a53b1b888430f9406503e9a5a0206f52
v-chen_data <v-chen_data@example.com>
2024-10-28 02:47:53 -0700

    cast

--- llmfoundry/models/llm_embed/modeling_llm_embed.py
+++ llmfoundry/models/llm_embed/modeling_llm_embed.py
@@ -30,7 +30,7 @@ from transformers.modeling_outputs import CausalLMOutputWithPast
 
 from llmfoundry import registry
 from llmfoundry.models.hf.hf_causal_lm import ComposerHFCausalLM
-from llmfoundry.models.mpt import ComposerMPTCausalLM
+from llmfoundry.models.mpt import MPTForCausalLM
 from llmfoundry.models.utils.config_moe_args import create_set_process_group
 
 log = logging.getLogger(__name__)
@@ -212,7 +212,7 @@ class ContrastiveModel(HuggingFaceModel):
             )
         else:
             model_class = registry.models.get('mpt_causal_lm')
-            model_class = cast(type[ComposerMPTCausalLM], model_class)
+            model_class = cast(type[MPTForCausalLM], model_class)
             model = model_class(**self.kwargs)
             self.is_mpt = True
         return model

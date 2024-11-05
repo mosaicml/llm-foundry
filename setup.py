f5e2e106c94c8dc5bb122f323f374b622fe86639
Irene Dea <deaairene@gmail.com>
2024-11-04 21:01:58 -0800

    see if this passes ci

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ install_requires = [
     'mlflow>=2.14.1,<2.18',
     'accelerate>=0.25,<0.34',  # for HF inference `device_map`
     'transformers>=4.43.2,<4.44',
-    'mosaicml-streaming>=0.9.0,<0.10',
+    'mosaicml-streaming==0.9.0',
     'torch>=2.4.0,<2.4.1',
     'datasets>=2.19,<2.20',
     'fsspec==2023.6.0',  # newer version results in a bug in datasets that duplicates data

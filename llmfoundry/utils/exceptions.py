99dae6123560344621a32b08c61b6e3049f1b710
Milo Cress <milo.cress@databricks.com>
2024-11-12 18:36:34 +0000

    fix serialization

--- llmfoundry/utils/exceptions.py
+++ llmfoundry/utils/exceptions.py
@@ -318,7 +318,7 @@ class MalformedUCTableError(UserError):
 
     def __init__(self, message: str) -> None:
         message = message
-        super().__init__(message)
+        super().__init__(message=message)
 
 
 class ClusterDoesNotExistError(UserError):

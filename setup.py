0b3a028a63975769f18565602d5eb17c40465999
Mihir Patel <mihir.v.patel7@gmail.com>
2024-11-14 13:41:26 -0500

    Pin FAv2 (#1656)

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ extra_deps['tensorboard'] = [
 
 # Flash 2 group kept for backwards compatibility
 extra_deps['gpu-flash2'] = [
-    'flash-attn>=2.6.3,<3',
+    'flash-attn==2.6.3',
 ]
 
 extra_deps['gpu'] = copy.deepcopy(extra_deps['gpu-flash2'])

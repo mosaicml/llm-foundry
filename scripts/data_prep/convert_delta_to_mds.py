fd54b5958bd286202f5d4e5581da024220dd0aac
Matthew Ding <matthew.ding@databricks.com>
2024-11-24 22:36:08 -0800

    update script

--- scripts/data_prep/convert_delta_to_mds.py
+++ scripts/data_prep/convert_delta_to_mds.py
@@ -1,6 +1,7 @@
 # Copyright 2022 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
+import json
 import logging
 from argparse import ArgumentParser
 
@@ -23,10 +24,58 @@ if __name__ == '__main__':
         '--mds_output_folder',
         required=True,
         type=str,
-        help='Local path to save the converted MDS shards',
+        help='Local path to save the MDS shards',
+    )
+    parser.add_argument(
+        '--http_path',
+        required=False,
+        type=str,
+        help='http_path is set then dbsql method is used',
+    )
+    parser.add_argument(
+        "--batch_size",
+        required=False,
+        type=int,
+        default=1 << 30,
+        help='row chunks to transmit a time to avoid OOM',
+    )
+    parser.add_argument(
+        "--processes",
+        required=False,
+        type=int,
+        default=1,
+        help='number of processes allowed to use',
+    )
+    parser.add_argument(
+        "--cluster_id",
+        required=False,
+        type=str,
+        help='cluster id to use for serverless',
+    )
+    parser.add_argument(
+        "--use_serverless",
+        required=False,
+        action='store_true',
+        help='use serverless cluster',
+    )
+    parser.add_argument(
+        "--dtypes",
+        required=False,
+        type=str,
+        help='data types for columns',
     )
     args = parser.parse_args()
+    
+    if args.dtypes is not None:
+        args.dtypes = json.loads(args.dtypes)
+
     convert_delta_to_mds_from_args(
         delta_table_name=args.delta_table_name,
         mds_output_folder=args.mds_output_folder,
+        http_path=args.http_path,
+        cluster_id=args.cluster_id,
+        use_serverless=args.use_serverless,
+        batch_size=args.batch_size,
+        processes=args.processes,
+        dtypes=args.dtypes,
     )

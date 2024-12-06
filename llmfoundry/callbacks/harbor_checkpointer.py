84dab4a81308d79f2d7c6299f1b9346884e703da
Milo Cress <milo.cress@databricks.com>
2024-12-06 17:18:27 +0000

    skeleton oras

--- llmfoundry/callbacks/harbor_checkpointer.py
+++ llmfoundry/callbacks/harbor_checkpointer.py
@@ -1,2 +1,10 @@
 # Copyright 2024 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
+
+from llmfoundry.callbacks.hf_checkpointer import HuggingFaceCheckpointer
+
+# uses the oras client to save the model to the harbor registry
+
+
+class HarborCheckpointer(HuggingFaceCheckpointer):
+    pass

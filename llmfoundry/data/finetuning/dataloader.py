d194f4e8fb67d90718b6204eb3980e81cd8608e7
Chuck Tang <chuck.tang@databricks.com>
2024-12-23 23:04:58 -0800

    commit change

--- llmfoundry/data/finetuning/dataloader.py
+++ llmfoundry/data/finetuning/dataloader.py
@@ -303,7 +303,7 @@ def build_finetuning_dataloader(
             if k in dataset_constructor_keys and
             k not in {'split', 'preprocessing_fn'}
         }
-        print("Data constructer args", dataset_constructor_args)
+        print("Data constructer args", **dataset_constructor_args)
         streaming_dataset = dataset_constructor.build_from_hf(
             dataset_name=dataset_name_or_path,
             split=split,

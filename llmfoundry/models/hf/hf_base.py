c16058012e67d540194bdfa29a7856d066c0e39a
Vincent Chen <v-chen_data@example.com>
2025-03-11 15:01:50 -0700

    update error

--- llmfoundry/models/hf/hf_base.py
+++ llmfoundry/models/hf/hf_base.py
@@ -232,7 +232,9 @@ class BaseHuggingFaceModel(HuggingFaceModel):
             'mosaicml/mpt',
         ):
             raise ValueError(
-                'MPT models are no longer supported by LLM Foundry due to transformer version v4.49.0 incompatibilities'
+                'The MPT series of models on the Hugging Face Hub is no longer supported by LLM Foundry. ' +
+                'Please use an older version of LLM Foundry (<0.18) or use a different model. '  +
+                'Please open a GitHub issue and we can help you downgrade or work around the issue.'
             )
         # Resolve "mixed" init device to either "cpu" or "meta"
         resolved_init_device = hf_get_init_device(init_device)

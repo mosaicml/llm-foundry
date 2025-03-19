3c0a4833c41e04d8f894d79450844e483b47e8d1
jjuvonen-amd <joni.juvonen@amd.com>
2025-03-19 10:22:49 +0000

    use context manager to handle deterministic torch settings in unit test

--- tests/models/hf/test_hf_fsdp_meta.py
+++ tests/models/hf/test_hf_fsdp_meta.py
@@ -1,6 +1,8 @@
 # Copyright 2024 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
+from contextlib import contextmanager
+
 import pytest
 import torch
 from composer import Trainer
@@ -9,56 +11,73 @@ from llmfoundry.models.hf.hf_causal_lm import ComposerHFCausalLM
 from llmfoundry.utils.builders import build_tokenizer
 
 
+@contextmanager
+def deterministic_torch_context():
+    """Context manager for deterministic torch settings."""
+    # Save previous settings
+    prev_deterministic = torch.are_deterministic_algorithms_enabled()
+    prev_fill_uninitialized = torch.utils.deterministic.fill_uninitialized_memory
+
+    # Enable deterministic settings
+    torch.use_deterministic_algorithms(True)
+    torch.utils.deterministic.fill_uninitialized_memory = True
+
+    try:
+        yield
+    finally:
+        # Restore previous settings
+        torch.use_deterministic_algorithms(prev_deterministic)
+        torch.utils.deterministic.fill_uninitialized_memory = prev_fill_uninitialized
+
+
 @pytest.mark.gpu
 @pytest.mark.world_size(2)
 def test_hf_meta_init_fsdp():
     # Use deterministic mode to ensure uninitialized weights are filled with NaNs
-    torch.use_deterministic_algorithms(False)
-    torch.utils.deterministic.fill_uninitialized_memory = True
+    with deterministic_torch_context():
+        fsdp_config = {
+            'sharding_strategy': 'FULL_SHARD',
+            'mixed_precision': 'PURE',
+            'activation_checkpointing': False,
+            'activation_checkpointing_reentrant': False,
+            'activation_cpu_offload': False,
+            'limit_all_gathers': True,
+            'state_dict_type': 'full',
+            'sync_module_states': True,
+        }
 
-    fsdp_config = {
-        'sharding_strategy': 'FULL_SHARD',
-        'mixed_precision': 'PURE',
-        'activation_checkpointing': False,
-        'activation_checkpointing_reentrant': False,
-        'activation_cpu_offload': False,
-        'limit_all_gathers': True,
-        'state_dict_type': 'full',
-        'sync_module_states': True,
-    }
+        tokenizer_name = 'codellama/CodeLlama-7b-hf'
+        tokenizer = build_tokenizer(
+            tokenizer_name=tokenizer_name,
+            tokenizer_kwargs={'model_max_length': 32},
+        )
 
-    tokenizer_name = 'codellama/CodeLlama-7b-hf'
-    tokenizer = build_tokenizer(
-        tokenizer_name=tokenizer_name,
-        tokenizer_kwargs={'model_max_length': 32},
-    )
+        model = ComposerHFCausalLM(
+            tokenizer=tokenizer,
+            pretrained_model_name_or_path='codellama/CodeLlama-7b-hf',
+            pretrained_lora_id_or_path=None,
+            trust_remote_code=False,
+            init_device='meta',
+            use_flash_attention_2=False,
+            use_auth_token=False,
+            config_overrides={
+                'num_hidden_layers': 2,
+                'hidden_size': 32,
+                'intermediate_size': 64,
+            },
+            load_in_8bit=False,
+            pretrained=False,
+        )
 
-    model = ComposerHFCausalLM(
-        tokenizer=tokenizer,
-        pretrained_model_name_or_path='codellama/CodeLlama-7b-hf',
-        pretrained_lora_id_or_path=None,
-        trust_remote_code=False,
-        init_device='meta',
-        use_flash_attention_2=False,
-        use_auth_token=False,
-        config_overrides={
-            'num_hidden_layers': 2,
-            'hidden_size': 32,
-            'intermediate_size': 64,
-        },
-        load_in_8bit=False,
-        pretrained=False,
-    )
+        trainer = Trainer(
+            model=model,
+            device='gpu',
+            parallelism_config={'fsdp': fsdp_config},
+        )
+        assert trainer.state.fsdp_enabled
 
-    trainer = Trainer(
-        model=model,
-        device='gpu',
-        parallelism_config={'fsdp': fsdp_config},
-    )
-    assert trainer.state.fsdp_enabled
-
-    # Check for NaN weights
-    for name, param in trainer.state.model.model.named_parameters():
-        assert not torch.isnan(
-            param,
-        ).any(), f'NaN detected in parameter: {name}'
+        # Check for NaN weights
+        for name, param in trainer.state.model.model.named_parameters():
+            assert not torch.isnan(
+                param,
+            ).any(), f'NaN detected in parameter: {name}'

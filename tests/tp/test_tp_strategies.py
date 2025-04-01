3fa2b4d83ea2cc1ce815985fef656895a2439249
Daniel King <daniel@mosaicml.com>
2025-04-01 18:12:58 +0000

    pc

--- tests/tp/test_tp_strategies.py
+++ tests/tp/test_tp_strategies.py
@@ -168,7 +168,12 @@ def test_tp_train(
     # Compare loss and expected loss for TP
     import numpy as np
     expected_tp_loss = np.array([
-        11.846275, 11.854343, 11.881471, 11.873639, 11.814651, 11.809362
+        11.846275,
+        11.854343,
+        11.881471,
+        11.873639,
+        11.814651,
+        11.809362,
     ])
     np.testing.assert_allclose(tp_loss, expected_tp_loss)
 

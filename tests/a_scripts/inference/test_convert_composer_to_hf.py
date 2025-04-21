dfba2b3939d7baa53889241d53753972fa56738a
Ethan Tang <ethan.tang@databricks.com>
2025-04-21 11:17:50 -0700

    typing

--- tests/a_scripts/inference/test_convert_composer_to_hf.py
+++ tests/a_scripts/inference/test_convert_composer_to_hf.py
@@ -1876,7 +1876,7 @@ def test_save_additional_contents(request: pytest.FixtureRequest):
 
     class ExtendedHuggingFaceCheckpointer(HuggingFaceCheckpointer):
 
-        def __init__(self, *args: list[Any], **kwargs: dict[str, Any]):
+        def __init__(self, *args: Any, **kwargs: Any):
             super().__init__(*args, **kwargs)
             self._calls = 0
             self._save_dir_calls: list[str] = []

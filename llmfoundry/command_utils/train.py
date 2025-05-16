65b12517e252ec69a1cec1d2d64331cc9b70f08b
bowenyang008 <byang008@ucr.edu>
2025-05-15 20:11:24 -0700

    benchmark flag

--- llmfoundry/command_utils/train.py
+++ llmfoundry/command_utils/train.py
@@ -558,7 +558,8 @@ def train(cfg: DictConfig) -> Trainer:
     compile_config = train_cfg.compile_config
     # Build the Trainer
     log.info('Building trainer...')
-    trainer_start_time = time.time()
+    if int(os.environ.get('BENCHMARK_TRAINER', 0)) == 1:
+        trainer_start_time = time.time()
     trainer = Trainer(
         run_name=run_name,
         seed=seed,
@@ -601,9 +602,10 @@ def train(cfg: DictConfig) -> Trainer:
         accumulate_train_batch_on_tokens=train_cfg.
         accumulate_train_batch_on_tokens,
     )
-    trainer_end_time = time.time()
-    trainer_construction_time = trainer_end_time - trainer_start_time
-    log.info(f'Trainer construction took {trainer_construction_time:.2f} seconds')
+    if int(os.environ.get('BENCHMARK_TRAINER', 0)) == 1:
+        trainer_end_time = time.time()
+        trainer_construction_time = trainer_end_time - trainer_start_time
+        log.info(f'Trainer construction took {trainer_construction_time:.2f} seconds')
     _sort_callbacks(trainer)
 
     # Optionally just save an HF checkpoint
